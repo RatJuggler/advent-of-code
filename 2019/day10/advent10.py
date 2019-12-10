858cc050ca80a4a146c1670db33951139897a64b
John Chase <ratteal@gmail.com>
2019-12-10 16:49:47 +0000

    Refactor map loading and capture the map size.

--- 2019/day10/advent10.py
+++ 2019/day10/advent10.py
@@ -1,42 +1,38 @@
 def load_asteroid_map(filename):
-    asteroid_map = ''
+    asteroid_map = []
     with open(filename) as fh:
         while True:
             map_line = fh.readline()
             if not map_line:
                 break
-            asteroid_map += map_line
+            asteroid_map.append(map_line.rstrip('\n'))
     return asteroid_map
 
 
 def convert_map_to_coords(asteroid_map):
     coords = []
-    x = 0
-    y = 0
-    for c in asteroid_map:
-        if c == '#':
-            coords.append((x, y))
-        elif c == '\n':
-            y += 1
-            x = -1
-        elif c != '.':
-            raise Exception('Unknown map character {0}!'.format(c))
-        x += 1
-    return coords
+    for y in range(len(asteroid_map)):
+        map_line = asteroid_map[y]
+        for x in range(len(map_line)):
+            c = map_line[x]
+            if c == '#':
+                coords.append((x, y))
+            elif c != '.':
+                raise Exception('Unknown map character {0}!'.format(c))
+    return coords, (len(asteroid_map[0]), len(asteroid_map))
 
 
-def detect_asteroids(from_base):
+def detect_asteroids(asteroid_coords, map_size, from_base):
     return 0
 
 
-def find_best_base(asteroid_coords):
+def find_best_base(asteroid_coords, map_size):
     maximum_detected = 0
     best_base = None
     for base in asteroid_coords:
-        asteroids_detected = detect_asteroids(base)
-        if asteroids_detected == maximum_detected:
-            print('More than one solution found!')
-        elif asteroids_detected > maximum_detected:
+        asteroids_detected = detect_asteroids(asteroid_coords, map_size, base)
+        print('{0} observable count = {1}'.format(base, asteroids_detected))
+        if asteroids_detected > maximum_detected:
             maximum_detected = asteroids_detected
             best_base = base
     return maximum_detected, best_base
@@ -44,16 +40,16 @@ def find_best_base(asteroid_coords):
 
 def analyse_map(filename):
     asteroid_map = load_asteroid_map(filename)
-    asteroid_coords = convert_map_to_coords(asteroid_map)
-    return find_best_base(asteroid_coords)
+    asteroid_coords, map_size = convert_map_to_coords(asteroid_map)
+    return find_best_base(asteroid_coords, map_size)
 
 
 def test(filename, expected_location, expected_detected):
-    location, detected = analyse_map(filename)
+    detected, location = analyse_map(filename)
     assert location == expected_location, \
-        'Expected to find location {0} but found {1}!'.format(expected_location, location)
+        'For map {0} expected to find location {1} but found {2}!'.format(filename, expected_location, location)
     assert detected == expected_detected, \
-        'Expected to detect {0} asteroids but detected {1}!'.format(expected_detected, detected)
+        'For map {0} expected to detect {0} asteroids but detected {1}!'.format(filename, expected_detected, detected)
 
 
 def main():
@@ -63,8 +59,8 @@ def main():
     test('test10d.txt', (6, 3), 41)
     test('test10e.txt', (11, 13), 210)
 
-    location, detected = analyse_map('test10e.txt')
-    print('Day 10 Step 1, best location {1} detects {1} asteroids'.format(location, detected))
+    detected, location = analyse_map('test10e.txt')
+    print('Day 10 Step 1, best location {0} detects {1} asteroids'.format(location, detected))
 
 
 if __name__ == '__main__':
