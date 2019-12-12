2edec8d59b62455b355b0ba1f4341032acd419fe
John Chase <ratteal@gmail.com>
2019-12-12 08:41:17 +0000

    Add velocity attributes to moons.

--- 2019/day12/avent12.py
+++ 2019/day12/avent12.py
@@ -7,6 +7,9 @@ class Moon:
         self.x = int(x)
         self.y = int(y)
         self.z = int(z)
+        self.vel_x = 0
+        self.vel_y = 0
+        self.vel_z = 0
 
     @classmethod
     def from_string(cls, moon_coords):
@@ -15,7 +18,8 @@ class Moon:
         return Moon(coords[0], coords[1], coords[2])
 
     def __repr__(self):
-        return 'pos=<x={0}, y={1}, z={2}>'.format(self.x, self.y, self.z)
+        return 'pos=<x={0}, y={1}, z={2}>, vel=<x={3}, y={4}, z={5}>'\
+            .format(self.x, self.y, self.z, self.vel_x, self.vel_y, self.vel_z)
 
 
 def load_moon_start_positions(filename):
