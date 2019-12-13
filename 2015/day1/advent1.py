e0495d3ffeb512478275cae63227d735e17bfc19
John Chase <ratteal@gmail.com>
2019-12-13 17:28:29 +0000

    Define tests for step 2.

--- 2015/day1/advent1.py
+++ 2015/day1/advent1.py
@@ -21,6 +21,15 @@ def step1_load_and_follow_directions(filename):
     print('Day 1, Step 1 directions lead to floor {0}'.format(floor))
 
 
+def basement_step(directions):
+    return 0
+
+
+def simple_test_step2(directions, expected_step):
+    step = basement_step(directions)
+    assert step == expected_step, 'Expected to be in basement on step {0} but was step {1}!'.format(expected_step, step)
+
+
 def main():
     simple_test_step1('(())', 0)
     simple_test_step1('()()', 0)
@@ -32,6 +41,8 @@ def main():
     simple_test_step1(')))', -3)
     simple_test_step1(')())())', -3)
     step1_load_and_follow_directions('input1.txt')
+    simple_test_step2(')', 1)
+    simple_test_step2('()())', 5)
 
 
 if __name__ == '__main__':
