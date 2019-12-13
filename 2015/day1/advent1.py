a2fd1b754cb1aaa68fed4c880d8d5b279fae6f40
John Chase <ratteal@gmail.com>
2019-12-13 17:24:38 +0000

    Step 1 solution.

--- 2015/day1/advent1.py
+++ 2015/day1/advent1.py
@@ -1,22 +1,37 @@
 def follow_directions(directions):
-    return 0
+    floor = 0
+    for c in directions:
+        floor += 1 if c == '(' else -1 if c == ')' else 0
+    return floor
 
 
-def simple_test(directions, expected_floor):
+def simple_test_step1(directions, expected_floor):
     floor = follow_directions(directions)
     assert floor == expected_floor, 'Expected to be on floor {0} but now on floor {1}!'.format(expected_floor, floor)
 
 
+def load_directions(filename):
+    with open(filename) as f:
+        return f.readline()
+
+
+def step1_load_and_follow_directions(filename):
+    directions = load_directions(filename)
+    floor = follow_directions(directions)
+    print('Day 1, Step 1 directions lead to floor {0}'.format(floor))
+
+
 def main():
-    simple_test('(())', 0)
-    simple_test('()()', 0)
-    simple_test('(((', 3)
-    simple_test('(()(()(', 3)
-    simple_test('))(((((', 3)
-    simple_test('())', -1)
-    simple_test('))(', -1)
-    simple_test(')))', -3)
-    simple_test(')())())', -3)
+    simple_test_step1('(())', 0)
+    simple_test_step1('()()', 0)
+    simple_test_step1('(((', 3)
+    simple_test_step1('(()(()(', 3)
+    simple_test_step1('))(((((', 3)
+    simple_test_step1('())', -1)
+    simple_test_step1('))(', -1)
+    simple_test_step1(')))', -3)
+    simple_test_step1(')())())', -3)
+    step1_load_and_follow_directions('input1.txt')
 
 
 if __name__ == '__main__':
