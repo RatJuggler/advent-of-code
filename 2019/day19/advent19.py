dc6bdff0206d9c03019a9d331cdef645b9b8e430
John Chase <ratteal@gmail.com>
2019-12-19 16:20:49 +0000

    Refactoring and thinking.

--- 2019/day19/advent19.py
+++ 2019/day19/advent19.py
@@ -1,7 +1,7 @@
 from day19.intcode_processor import IntcodeProcessor
 
 
-def step1_points_affected_by_tractor_beam(size):
+def visualise(size):
     drone_control = IntcodeProcessor.from_file('input19.txt')
     affected = 0
     for y in range(size):
@@ -13,53 +13,56 @@ def step1_points_affected_by_tractor_beam(size):
                 c = '#'
                 affected += 1
             print(c, end='')
-        print(y)
-    print('Day 19, Step points affected found = {0}'.format(affected))
+        print(y, affected)
 
 
-def move_drone(x, y):
-    drone_control = IntcodeProcessor.from_file('input19.txt')
+def move_drone(drone_control, x, y):
+    drone_control.reset()
     output = drone_control.run([x, y])
     return output[0]
 
 
-def scan_for_affected(x, y):
+def scan_for_affected(drone_control, x, y, max_columns):
     drone_status = 0
-    while drone_status == 0 and x < 1000:
-        drone_status = move_drone(x, y)
-        if drone_status == 0:
-            x += 1
-    return 0 if x == 1000 else x
-
-
-def count_affected(x_from, y):
-    affected = 0
-    x = -1
-    drone_status = 1
-    while drone_status == 1:
+    while drone_status == 0 and x <= max_columns:
         x += 1
-        drone_status = move_drone(x_from + x, y)
-        if drone_status == 1:
-            affected += 1
+        drone_status = move_drone(drone_control, x, y)
+    return -1 if x > max_columns else x
+
+
+def count_affected(drone_control, x_from, y, max_columns):
+    affected = 0
+    x = 0
+    drone_status = 1
+    while drone_status == 1 and x_from + x < max_columns:
+        affected += 1
+        x += 1
+        drone_status = move_drone(drone_control, x_from + x, y)
     return affected
 
 
-def step2_find_closest_square(size):
+def step1_points_affected_by_tractor_beam(size):
+    drone_control = IntcodeProcessor.from_file('input19.txt')
     x = 0
-    y = -1
-    column_affected = 0
-    while column_affected < size:
-        y += 1
+    y = 0
+    total_affected = 0
+    while y < size:
         print('Scanning row {0} from {1}...'.format(y, x), end='')
-        x = scan_for_affected(x, y)
-        column_affected = count_affected(x, y)
-        print('Found {0}'.format(column_affected))
-    print(x, y)
+        x = scan_for_affected(drone_control, x - 1, y, size)
+        if x == -1:
+            columns_affected = 0
+        else:
+            columns_affected = count_affected(drone_control, x, y, size)
+        print('Found {0}'.format(columns_affected))
+        total_affected += columns_affected
+        y += 1
+    print('Day 19, Step points affected found = {0}'.format(total_affected))
 
 
 def main():
-    step1_points_affected_by_tractor_beam(100)
-    step2_find_closest_square(10)
+    visualise(50)
+    step1_points_affected_by_tractor_beam(50)
+    # step2_find_closest_square(10)
 
 
 if __name__ == '__main__':
