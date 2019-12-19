59cf7eceb12d056e266fb60b33919ac64c48eae6
John Chase <ratteal@gmail.com>
2019-12-19 17:21:42 +0000

    Brute force attempt.

--- 2019/day19/advent19.py
+++ 2019/day19/advent19.py
@@ -61,33 +61,46 @@ def step1_points_affected_by_tractor_beam(size):
 
 
 def square_test(rows_affected, size):
-    return len(rows_affected) < size
+    if len(rows_affected) < size:
+        return True
+    square_first_row = rows_affected[0]
+    first_row_end = square_first_row[0] + square_first_row[2]
+    square_last_row = rows_affected[size - 1]
+    last_row_start = square_last_row[0]
+    return first_row_end - last_row_start < size
 
 
 def step2_find_closest_square(size):
     drone_control = IntcodeProcessor.from_file('input19.txt')
     x = 0
     y = 0
+    max_columns = 10000
     rows_affected = []
     while square_test(rows_affected, size):
         print('Scanning row {0} from {1}...'.format(y, x), end='')
-        x = scan_for_affected(drone_control, x - 1, y, 1000)
+        x = scan_for_affected(drone_control, x - 1, y, max_columns)
         if x == -1:
             x = 0
             columns_affected = 0
         else:
-            columns_affected = count_affected(drone_control, x, y, 1000)
+            columns_affected = count_affected(drone_control, x, y, max_columns)
         print('Found {0} from {1}'.format(columns_affected, x))
         if columns_affected >= size:
-            rows_affected.append((x, columns_affected))
+            rows_affected.append((x, y, columns_affected))
+            if len(rows_affected) > size:
+                rows_affected.pop(0)
         y += 1
     print(rows_affected)
+    first_row = rows_affected[0]
+    value = first_row[0] * 10000 + first_row[1]
+    print('Day 19, Step 2 square size {0} starts at ({1}, {2}) giving value {3}'
+          .format(size, first_row[0], first_row[1], value))
 
 
 def main():
     # visualise(50)
     # step1_points_affected_by_tractor_beam(50)
-    step2_find_closest_square(10)
+    step2_find_closest_square(100)
 
 
 if __name__ == '__main__':
