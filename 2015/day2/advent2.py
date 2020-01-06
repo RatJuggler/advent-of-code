48f43efa9ddd65b83a7feaf0aae9af162a26c04d
John Chase <ratteal@gmail.com>
2020-01-06 16:36:20 +0000

    Step 2 solution.

--- 2015/day2/advent2.py
+++ 2015/day2/advent2.py
@@ -30,7 +30,12 @@ def step1_calculate_total_paper_required(filename):
 
 
 def calculate_ribbon_required(present_dimensions):
-    return 0
+    l, w, h = parse_dimensions(present_dimensions)
+    face1 = 2 * (l + w)
+    face2 = 2 * (w + h)
+    face3 = 2 * (h + l)
+    ribbon_required = min(face1, face2, face3) + (l * w * h)
+    return ribbon_required
 
 
 def simple_test_step2(present_dimensions, expected_ribbon_required):
@@ -39,12 +44,22 @@ def simple_test_step2(present_dimensions, expected_ribbon_required):
         'Expected to need {0} ft of ribbon for a present of dimensions {1} but calculated {2}!'\
         .format(expected_ribbon_required, present_dimensions, ribbon_required)
 
+
+def step2_calculate_total_ribbon_required(filename):
+    total_ribbon_required = 0
+    with open(filename) as fh:
+        for present_dimensions in fh:
+            total_ribbon_required += calculate_ribbon_required(present_dimensions)
+    print('Day 2, Step 2 ribbon paper required = {0} ft'.format(total_ribbon_required))
+
+
 def main():
     simple_test_step1('2x3x4', 58)
     simple_test_step1('1x1x10', 43)
     step1_calculate_total_paper_required('input2.txt')
     simple_test_step2('2x3x4', 34)
     simple_test_step2('1x1x10', 14)
+    step2_calculate_total_ribbon_required('input2.txt')
 
 
 if __name__ == '__main__':
