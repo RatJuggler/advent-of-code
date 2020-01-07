a6923de900c1237d60c3bd87499fcf401e2bf359
John Chase <ratteal@gmail.com>
2020-01-07 17:48:39 +0000

    Step 2 solution.

--- 2015/day5/advent5.py
+++ 2015/day5/advent5.py
@@ -5,9 +5,10 @@ def contains_three_vowels(string):
     return vowel_count > 2
 
 
-def contains_at_least_one_double_letter(string):
-    for i in range(len(string) - 1):
-        if string[i] == string[i + 1]:
+def repeating_letter_with_separator(string, separator):
+    separator += 1
+    for i in range(len(string) - separator):
+        if string[i] == string[i + separator]:
             return True
     return False
 
@@ -18,7 +19,7 @@ def does_not_contain(string):
 
 def step1_is_nice_string(string):
     return contains_three_vowels(string) and \
-           contains_at_least_one_double_letter(string) and \
+           repeating_letter_with_separator(string, 0) and \
            does_not_contain(string)
 
 
@@ -41,8 +42,15 @@ def step1_count_nice_strings(filename):
     print('Day 5, Step 1 nice strings found {0}.'.format(nice_strings))
 
 
+def contains_non_overlapping_pair(string):
+    for i in range(len(string) - 4):
+        pair = string[i:i + 2]
+        if string.find(pair, i + 2) != -1:
+            return True
+    return False
+
 def step2_is_nice_string(string):
-    return True
+    return contains_non_overlapping_pair(string) and repeating_letter_with_separator(string, 1)
 
 
 def simple_test_step2(string, expected_nice):
