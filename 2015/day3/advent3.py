8aa806cf0fff43fa71ae8c538b99c93fc06f7a85
John Chase <ratteal@gmail.com>
2020-01-07 13:56:46 +0000

    Step 1 solution.

--- 2015/day3/advent3.py
+++ 2015/day3/advent3.py
@@ -1,10 +1,17 @@
 def follow_directions(directions):
     x, y = 0, 0
+    visited_houses = {(x, y): 1}
     houses_visited = 1
     for c in directions:
         x += 1 if c == '>' else -1 if c == '<' else 0
         y += 1 if c == '^' else -1 if c == 'v' else 0
-        houses_visited += 1
+        house = (x, y)
+        house_visits = visited_houses.get(house)
+        if house_visits is None:
+            visited_houses[house] = 1
+            houses_visited += 1
+        else:
+            visited_houses[house] = house_visits + 1
     return houses_visited
 
 
@@ -14,10 +21,22 @@ def simple_test_step1(directions, expected_houses_visited):
         'Expected to visit {0} houses but was {1}!'.format(expected_houses_visited, houses_visited)
 
 
+def load_directions(filename):
+    with open(filename) as f:
+        return f.readline()
+
+
+def step1_count_houses_visited(filename):
+    directions = load_directions(filename)
+    houses_visited = follow_directions(directions)
+    print('Day 3, Step 1 houses visited is {0}'.format(houses_visited))
+
+
 def main():
     simple_test_step1('>', 2)
     simple_test_step1('^>v<', 4)
     simple_test_step1('^v^v^v^v^v', 2)
+    step1_count_houses_visited('input3.txt')
 
 
 if __name__ == '__main__':
