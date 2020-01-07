13161735124fc51f3c2a1151163c0579d840c195
John Chase <ratteal@gmail.com>
2020-01-07 13:59:42 +0000

    Tests for Step 2.

--- 2015/day3/advent3.py
+++ 2015/day3/advent3.py
@@ -32,11 +32,20 @@ def step1_count_houses_visited(filename):
     print('Day 3, Step 1 houses visited is {0}'.format(houses_visited))
 
 
+def simple_test_step2(directions, expected_houses_visited):
+    houses_visited = follow_directions(directions)
+    assert houses_visited == expected_houses_visited, \
+        'Expected to visit {0} houses but was {1}!'.format(expected_houses_visited, houses_visited)
+
+
 def main():
     simple_test_step1('>', 2)
     simple_test_step1('^>v<', 4)
     simple_test_step1('^v^v^v^v^v', 2)
     step1_count_houses_visited('input3.txt')
+    simple_test_step2('^v', 3)
+    simple_test_step2('^>v<', 3)
+    simple_test_step2('^v^v^v^v^v', 11)
 
 
 if __name__ == '__main__':
