0ad77d3f4cbe9888974a03088571ff49751fb62f
John Chase <ratteal@gmail.com>
2020-01-07 14:47:36 +0000

    Step 2 solution.

--- 2015/day3/advent3.py
+++ 2015/day3/advent3.py
@@ -1,17 +1,26 @@
+from itertools import zip_longest
+
+
+def move_and_visit(position, direction, visited_houses, houses_visited):
+    x, y = position[0], position[1]
+    x += 1 if direction == '>' else -1 if direction == '<' else 0
+    y += 1 if direction == '^' else -1 if direction == 'v' else 0
+    new_position = (x, y)
+    house_visits = visited_houses.get(new_position)
+    if house_visits is None:
+        visited_houses[new_position] = 1
+        houses_visited += 1
+    else:
+        visited_houses[new_position] = house_visits + 1
+    return new_position, visited_houses, houses_visited
+
+
 def follow_directions(directions):
-    x, y = 0, 0
-    visited_houses = {(x, y): 1}
+    position = (0, 0)
+    visited_houses = {position: 1}
     houses_visited = 1
-    for c in directions:
-        x += 1 if c == '>' else -1 if c == '<' else 0
-        y += 1 if c == '^' else -1 if c == 'v' else 0
-        position = (x, y)
-        house_visits = visited_houses.get(position)
-        if house_visits is None:
-            visited_houses[position] = 1
-            houses_visited += 1
-        else:
-            visited_houses[position] = house_visits + 1
+    for direction in directions:
+        position, visited_houses, houses_visited = move_and_visit(position, direction, visited_houses, houses_visited)
     return houses_visited
 
 
@@ -32,15 +41,35 @@ def step1_count_houses_visited(filename):
     print('Day 3, Step 1 houses visited by Santa is {0}'.format(houses_visited))
 
 
+def grouper(iterable, n, fillvalue=None):
+    # Collect data into fixed-length chunks or blocks
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def santa_and_robo_santa_follow_directions(directions):
+    santa_position = (0, 0)
+    robo_santa_position = (0, 0)
+    visited_houses = {santa_position: 2}
+    houses_visited = 1
+    for direction in grouper(directions, 2):
+        santa_position, visited_houses, houses_visited = \
+            move_and_visit(santa_position, direction[0], visited_houses, houses_visited)
+        robo_santa_position, visited_houses, houses_visited = \
+            move_and_visit(robo_santa_position, direction[1], visited_houses, houses_visited)
+    return houses_visited
+
+
 def simple_test_step2(directions, expected_houses_visited):
-    houses_visited = follow_directions(directions)
+    houses_visited = santa_and_robo_santa_follow_directions(directions)
     assert houses_visited == expected_houses_visited, \
         'Expected Santa and Robo-Santa to visit {0} houses but was {1}!'.format(expected_houses_visited, houses_visited)
 
 
 def step2_count_houses_visited(filename):
     directions = load_directions(filename)
-    houses_visited = follow_directions(directions)
+    houses_visited = santa_and_robo_santa_follow_directions(directions)
     print('Day 3, Step 2 houses visited by Santa and Robo-Santa is {0}'.format(houses_visited))
 
 
