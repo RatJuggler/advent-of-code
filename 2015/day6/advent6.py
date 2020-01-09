da38204ec0e651c7eff5b3e56597b93c914bc3fd
John Chase <ratteal@gmail.com>
2020-01-09 19:50:17 +0000

    Step 2 solution.

--- 2015/day6/advent6.py
+++ 2015/day6/advent6.py
@@ -15,33 +15,26 @@ class LightGrid:
             for x in range(x_start, x_start + x_range):
                 self.grid[y][x] = instruction.apply_action(self.grid[y][x])
 
-    def count_lights(self):
+    def count_brightness(self):
         lights_lit = 0
         for grid_row in self.grid:
             for light in grid_row:
                 lights_lit += light
         return lights_lit
 
-    def display(self):
-        display = ''
-        for grid_row in self.grid:
-            for light in grid_row:
-                display += 'X' if light == 1 else ' '
-            display += '\n'
-        print(display)
-
 
 class Instruction:
 
-    def __init__(self, action, corner1, corner2):
+    def __init__(self, action, corner1, corner2, step):
         self.action = action
         self.corner1 = corner1
         self.corner2 = corner2
+        self.step = step
 
     @classmethod
-    def from_line(cls, line):
+    def from_line(cls, line, step):
         action, corner1, corner2 = cls.decode_instruction(line)
-        return Instruction(action, corner1, corner2)
+        return Instruction(action, corner1, corner2, step)
 
     @staticmethod
     def decode_instruction(line):
@@ -63,6 +56,12 @@ class Instruction:
         return x_start, x_range, y_start, y_range
 
     def apply_action(self, light):
+        if self.step == 1:
+            return self.apply_action_step1(light)
+        if self.step == 2:
+            return self.apply_action_step2(light)
+
+    def apply_action_step1(self, light):
         if self.action == 'toggle':
             return 1 if light == 0 else 0
         elif self.action == 'turn on':
@@ -70,32 +69,49 @@ class Instruction:
         elif self.action == 'turn off':
             return 0
 
+    def apply_action_step2(self, light):
+        if self.action == 'toggle':
+            return light + 2
+        elif self.action == 'turn on':
+            return light + 1
+        elif self.action == 'turn off':
+            return light - 1 if light > 0 else 0
 
-def apply_instructions(grid, filename):
+
+def apply_instructions(grid, filename, step):
     with open(filename) as fh:
         for line in fh:
-            instruction = Instruction.from_line(line)
+            instruction = Instruction.from_line(line, step)
             grid.set_lights(instruction)
 
 
-def light_grid(filename):
+def light_grid(filename, step):
     grid = LightGrid(1000)
-    apply_instructions(grid, filename)
-    grid.display()
-    return grid.count_lights()
+    apply_instructions(grid, filename, step)
+    return grid.count_brightness()
 
 
 def step1_simple_test(filename, expected_lights_lit):
-    lights_lit = light_grid(filename)
+    lights_lit = light_grid(filename, 1)
     assert lights_lit == expected_lights_lit, \
         'Expected {0} lights to be lit but found {1}!'.format(expected_lights_lit, lights_lit)
+
+
+def step2_simple_test(filename, expected_lights_lit):
+    lights_lit = light_grid(filename, 2)
+    assert lights_lit == expected_lights_lit, \
+        'Expected brightness to be {0} but found {1}!'.format(expected_lights_lit, lights_lit)
 
 
 def main():
     step1_simple_test('test6a.txt', 13)
     step1_simple_test('test6b.txt', 999000)
-    lights_lit = light_grid('input6.txt')
+    lights_lit = light_grid('input6.txt', 1)
     print('Day 6, Step 1 lights lit = {0}'.format(lights_lit))
+    step2_simple_test('test6a.txt', 13)
+    step2_simple_test('test6b.txt', 1002000)
+    lights_lit = light_grid('input6.txt', 2)
+    print('Day 6, Step 2 brightness = {0}'.format(lights_lit))
 
 
 if __name__ == '__main__':
