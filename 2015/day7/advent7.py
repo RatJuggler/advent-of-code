7578acbc153d68df3cb1c796c4836c531e8dc802
John Chase <ratteal@gmail.com>
2020-01-10 14:23:43 +0000

    Read the instructions for each wire into a dictionary.

--- 2015/day7/advent7.py
+++ 2015/day7/advent7.py
@@ -1,4 +1,41 @@
+import re
+
+
+class Instruction:
+
+    def __init__(self, wire, connection):
+        self.wire = wire
+        self.connection = connection
+
+    @classmethod
+    def from_line(cls, line):
+        wire, connection = cls.decode_instruction(line)
+        return Instruction(wire, connection)
+
+    @staticmethod
+    def decode_instruction(line):
+        regex = r'(.+) -> (.+)'
+        matches = re.findall(regex, line)
+        wire = matches[0][1]
+        connection = matches[0][0]
+        return wire, connection
+
+    def __repr__(self):
+        return "Instruction({0}, {1})".format(self.wire, self.connection)
+
+
+def load_circuit(filename):
+    circuit = {}
+    with open(filename) as fh:
+        for line in fh:
+            instruction = Instruction.from_line(line)
+            circuit[instruction.wire] = instruction
+    return circuit
+
+
 def evaluate_circuit(filename):
+    circuit = load_circuit(filename)
+    print(circuit)
     return {}
 
 
