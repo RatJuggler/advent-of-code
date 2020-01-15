702920f4852c788b80837c76a341d889882f53a5
John Chase <ratteal@gmail.com>
2020-01-15 16:22:47 +0000

    Start by holding the list of strings in an enclosing class.

--- 2015/day8/advent8.py
+++ 2015/day8/advent8.py
@@ -1,15 +1,42 @@
-def character_count_difference(filename):
-    return 0
+class SanatasList:
+
+    def __init__(self, entries):
+        self.entries = entries
+
+    @classmethod
+    def from_file(cls, filename):
+        entries = []
+        with open(filename) as fh:
+            for line in fh:
+                entries.append(line)
+        return SanatasList(entries)
+
+    def code_string_lengths(self):
+        total_length = 0
+        for line in self.entries:
+            total_length += len(line)
+        return total_length
+
+    def in_memory_string_lengths(self):
+        total_length = 0
+        for line in self.entries:
+            total_length += len(line)
+        return total_length
 
 
-def test_character_count_difference(filename, expected_difference):
-    difference = character_count_difference(filename)
+def string_length_difference(filename):
+    list = SanatasList.from_file(filename)
+    return list.code_string_lengths() - list.in_memory_string_lengths()
+
+
+def test_string_count_difference(filename, expected_difference):
+    difference = string_length_difference(filename)
     assert difference == expected_difference, \
         'Expected difference to be {0} but was {1}!'.format(expected_difference, difference)
 
 
 def main():
-    test_character_count_difference('test8a.py', 12)
+    test_string_count_difference('test8a.txt', 12)
 
 
 if __name__ == '__main__':
