f5e62749d5b20ea6a058609d5ca4c67a8b2fab15
John Chase <ratteal@gmail.com>
2020-01-21 17:26:05 +0000

    Simple array summing tests pass.

--- 2015/day12/advent12.py
+++ 2015/day12/advent12.py
@@ -1,11 +1,19 @@
-def sum_array(array):
-    return 0
+def sum_array(array: list):
+    array_sum = 0
+    for element in array:
+        print(element, type(element))
+        if type(element) is list:
+            array_sum += sum_array(element)
+        else:
+            array_sum += element
+    return array_sum
 
 
-def test_sum_arrays(array, expected_sum):
-    array_sum = sum_array(array)
+def test_sum_arrays(array_string: str, expected_sum):
+    array_list = eval(array_string)
+    array_sum = sum_array(array_list)
     assert array_sum == expected_sum, \
-        'Expected sum of array {0} to be {1} but was {2}'.format(array, expected_sum, array_sum)
+        'Expected sum of array {0} to be {1} but was {2}'.format(array_string, expected_sum, array_sum)
 
 
 def main():
