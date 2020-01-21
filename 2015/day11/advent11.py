5061f8df7a36490487b2c4a52c09c699284caf7d
John Chase <ratteal@gmail.com>
2020-01-21 10:32:47 +0000

    Start to build up password validation.

--- 2015/day11/advent11.py
+++ 2015/day11/advent11.py
@@ -1,5 +1,38 @@
+import re
+
+
+def is_eight_letters(password):
+    regex = r'^[a-z]{8}$'
+    matches = re.match(regex, password)
+    return matches is not None
+
+
+def contains_iol(password):
+    regex = r'[iol]'
+    matches = re.findall(regex, password)
+    return len(matches) > 0
+
+
+def contains_sequence(password):
+    sequence = 0
+    for c in range(len(password) - 1):
+        if ord(password[c + 1]) != ord(password[c]) + 1:
+            sequence = 0
+        elif sequence == 2:
+            return True
+        else:
+            sequence += 1
+    return False
+
+
 def valid_password(password):
-    return password == password
+    if not is_eight_letters(password):
+        return False
+    if contains_iol(password):
+        return False
+    if not contains_sequence(password):
+        return False
+    return True
 
 
 def generate_next_password(password):
@@ -19,9 +52,14 @@ def test_next_password(password, expected_next_password):
 
 
 def main():
+    test_valid_password('abcddee', False)
+    test_valid_password('12345678', False)
     test_valid_password('hijklmmn', False)
     test_valid_password('abbceffg', False)
-    test_valid_password('abbcegjk', True)
+    test_valid_password('abbcegjk', False)
+    test_valid_password('abcxxkkq', True)
+    test_valid_password('abcdffaa', True)
+    test_valid_password('ghjaabcc', True)
     test_next_password('abcdefgh', 'abcdffaa')
     test_next_password('ghijklmn', 'ghjaabcc')
 
