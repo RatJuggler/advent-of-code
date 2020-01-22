868b65ac86a095c41d69d6e33a72fa0006301d00
John Chase <ratteal@gmail.com>
2020-01-22 16:00:43 +0000

    Refactor to simpler structure without classes.

--- 2015/day13/advent13.py
+++ 2015/day13/advent13.py
@@ -2,45 +2,29 @@ import re
 import itertools
 
 
-class HappinessNextTo:
-
-    def __init__(self, person_a, person_b, happiness):
-        self.person_a = person_a
-        self.person_b = person_b
-        self.happiness = happiness
-
-    @classmethod
-    def from_line(cls, line):
-        person_a, person_b, happiness = cls.decode_line(line)
-        return HappinessNextTo(person_a, person_b, happiness)
-
-    @staticmethod
-    def decode_line(line):
-        regex = r'^(?P<person_a>\S+) would (?P<difference>\S+) (?P<happiness>\d+) ' \
-                r'happiness units by sitting next to (?P<person_b>\S+)\.'
-        matches = re.match(regex, line)
-        person_a = matches.group('person_a')
-        person_b = matches.group('person_b')
-        difference = matches.group('difference')
-        happiness = int(matches.group('happiness'))
-        if difference == 'lose':
-            happiness = -happiness
-        return person_a, person_b, happiness
-
-    def __repr__(self):
-        return "Happiness({0} next to {1} = {2})".format(self.person_a, self.person_b, self.happiness)
+def decode_line(line):
+    regex = r'^(?P<person_a>\S+) would (?P<difference>\S+) (?P<happiness>\d+) ' \
+            r'happiness units by sitting next to (?P<person_b>\S+)\.'
+    matches = re.match(regex, line)
+    person_a = matches.group('person_a')
+    person_b = matches.group('person_b')
+    difference = matches.group('difference')
+    happiness = int(matches.group('happiness'))
+    if difference == 'lose':
+        happiness = -happiness
+    return person_a, person_b, happiness
 
 
 def load_seating(filename):
     people = {}
     with open(filename) as fh:
         for line in fh:
-            next_to = HappinessNextTo.from_line(line)
-            person = people.get(next_to.person_a)
+            person_a, person_b, happiness = decode_line(line)
+            person = people.get(person_a)
             if person is None:
-                person = []
-            person.append(next_to)
-            people[next_to.person_a] = person
+                person = {}
+            person[person_b] = happiness
+            people[person_a] = person
     return people
 
 
