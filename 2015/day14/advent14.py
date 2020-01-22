0059812b928d5953cdb34193ca41b8a756d3d2ab
John Chase <ratteal@gmail.com>
2020-01-22 18:22:20 +0000

    Load reindeet data.

--- 2015/day14/advent14.py
+++ 2015/day14/advent14.py
@@ -1,4 +1,47 @@
+import re
+
+
+class Reindeer:
+
+    def __init__(self, name, speed, duration, rest):
+        self.name = name
+        self.speed = speed
+        self.duration = duration
+        self.rest = rest
+
+    @classmethod
+    def from_line(cls, line):
+        name, speed, duration, rest = cls.decode_line(line)
+        return Reindeer(name, speed, duration, rest)
+
+    @staticmethod
+    def decode_line(line):
+        regex = r'(?P<name>\S+) can fly (?P<speed>\d+) km\/s for (?P<duration>\d+) seconds, ' \
+                r'but then must rest for (?P<rest>\d+) seconds\.'
+        matches = re.match(regex, line)
+        name = matches.group('name')
+        speed = matches.group('speed')
+        duration = int(matches.group('duration'))
+        rest = int(matches.group('rest'))
+        return name, speed, duration, rest
+
+    def __repr__(self):
+        return "Reindeer(Name: {0}, Speed: {1} km/s, Duration: {2}, Rest: {3})"\
+            .format(self.name, self.speed, self.duration, self.rest)
+
+
+def load_reindeer(filename):
+    reindeer = []
+    with open(filename) as fh:
+        for line in fh:
+            reindeer.append(Reindeer.from_line(line))
+    return reindeer
+
+
 def find_winning_distance(filename, after):
+    reindeer = load_reindeer(filename)
+    for a_reindeer in reindeer:
+        print(a_reindeer)
     return 0
 
 
