3447bc1548bcffff9dd0600a10cc917a92787090
John Chase <ratteal@gmail.com>
2020-01-24 11:09:30 +0000

    Review, make better use function parameter and return types.

--- 2015/day1/advent1.py
+++ 2015/day1/advent1.py
@@ -1,27 +1,27 @@
-def follow_directions(directions):
+def follow_directions(directions: str) -> int:
     floor = 0
     for c in directions:
         floor += 1 if c == '(' else -1 if c == ')' else 0
     return floor
 
 
-def simple_test_step1(directions, expected_floor):
+def simple_test_step1(directions: str, expected_floor: int) -> None:
     floor = follow_directions(directions)
     assert floor == expected_floor, 'Expected to be on floor {0} but now on floor {1}!'.format(expected_floor, floor)
 
 
-def load_directions(filename):
+def load_directions(filename: str) -> str:
     with open(filename) as f:
         return f.readline()
 
 
-def step1_load_and_follow_directions(filename):
+def step1_load_and_follow_directions(filename: str) -> None:
     directions = load_directions(filename)
     floor = follow_directions(directions)
     print('Day 1, Step 1 directions lead to floor {0}'.format(floor))
 
 
-def basement_step(directions):
+def basement_step(directions: str) -> int:
     floor = 0
     for step, c in enumerate(directions):
         floor += 1 if c == '(' else -1 if c == ')' else 0
@@ -30,18 +30,18 @@ def basement_step(directions):
     return -1
 
 
-def simple_test_step2(directions, expected_step):
+def simple_test_step2(directions: str, expected_step: int) -> None:
     step = basement_step(directions)
     assert step == expected_step, 'Expected to be in basement on step {0} but was step {1}!'.format(expected_step, step)
 
 
-def step2_load_and_follow_directions(filename):
+def step2_load_and_follow_directions(filename: str) -> None:
     directions = load_directions(filename)
     step = basement_step(directions)
     print('Day 1, Step 2 basement reached on step {0}'.format(step))
 
 
-def main():
+def main() -> None:
     simple_test_step1('(())', 0)
     simple_test_step1('()()', 0)
     simple_test_step1('(((', 3)
