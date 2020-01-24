b4f9097089a9c24a1291394a050f05bfdb554616
John Chase <ratteal@gmail.com>
2020-01-24 20:11:58 +0000

    Review, make better use of function parameter and return types.

--- 2015/day8/advent8.py
+++ 2015/day8/advent8.py
@@ -1,56 +1,59 @@
+from typing import Callable, List
+
+
 class SanatasList:
 
-    def __init__(self, entries):
+    def __init__(self, entries: List[str]) -> None:
         self.entries = entries
 
     @classmethod
-    def from_file(cls, filename):
+    def from_file(cls, filename: str):
         entries = []
         with open(filename) as fh:
             for line in fh:
                 entries.append(line.strip())
         return SanatasList(entries)
 
-    def string_lengths(self, calc_length):
+    def string_lengths(self, calc_length: Callable[[str], int]) -> int:
         total_length = 0
         for line in self.entries:
             total_length += calc_length(line)
         return total_length
 
-    def code_string_lengths(self):
+    def code_string_lengths(self) -> int:
         return self.string_lengths(lambda s: len(s))
 
-    def in_memory_string_lengths(self):
+    def in_memory_string_lengths(self) -> int:
         return self.string_lengths(lambda s: len(eval(s)))
 
-    def encoded_string_length(self):
+    def encoded_string_length(self) -> int:
         return self.string_lengths(
             lambda s: len(s) + 4 + (2 * (s.count('\"') - 2)) + (s.count('\\') + 2 - s.count('\"')))
 
 
-def step1_string_length_difference(filename):
+def step1_string_length_difference(filename: str) -> int:
     santas_list = SanatasList.from_file(filename)
     return santas_list.code_string_lengths() - santas_list.in_memory_string_lengths()
 
 
-def step2_string_length_difference(filename):
+def step2_string_length_difference(filename: str) -> int:
     santas_list = SanatasList.from_file(filename)
     return santas_list.encoded_string_length() - santas_list.code_string_lengths()
 
 
-def test_test1_string_count_difference(filename, expected_difference):
+def test_test1_string_count_difference(filename: str, expected_difference: int) -> None:
     difference = step1_string_length_difference(filename)
     assert difference == expected_difference, \
         'Expected difference to be {0} but was {1}!'.format(expected_difference, difference)
 
 
-def test_test2_string_count_difference(filename, expected_difference):
+def test_test2_string_count_difference(filename: str, expected_difference: int) -> None:
     difference = step2_string_length_difference(filename)
     assert difference == expected_difference, \
         'Expected difference to be {0} but was {1}!'.format(expected_difference, difference)
 
 
-def main():
+def main() -> None:
     test_test1_string_count_difference('test8a.txt', 12)
     print('Day 8, Step 1 difference = {0}'.format((step1_string_length_difference('input8.txt'))))
     test_test2_string_count_difference('test8a.txt', 19)
