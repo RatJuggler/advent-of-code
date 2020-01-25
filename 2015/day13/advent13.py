d618dd4917eb4fe534d2483f3b43c2219fee3b03
John Chase <ratteal@gmail.com>
2020-01-25 16:14:23 +0000

    Review, make better use of function parameter and return types.

--- 2015/day13/advent13.py
+++ 2015/day13/advent13.py
@@ -1,8 +1,9 @@
+from typing import Dict
 import re
 import itertools
 
 
-def decode_line(line):
+def decode_line(line: str) -> [str, str, int]:
     regex = r'^(?P<person_a>\S+) would (?P<difference>\S+) (?P<happiness>\d+) ' \
             r'happiness units by sitting next to (?P<person_b>\S+)\.'
     matches = re.match(regex, line)
@@ -15,7 +16,7 @@ def decode_line(line):
     return person_a, person_b, happiness
 
 
-def load_seating(filename):
+def load_seating(filename: str) -> Dict[str, Dict[str, int]]:
     people = {}
     with open(filename) as fh:
         for line in fh:
@@ -28,7 +29,7 @@ def load_seating(filename):
     return people
 
 
-def plan_seating(people):
+def plan_seating(people: Dict[str, Dict[str, int]]) -> int:
     most_happiness = 0
     places = len(people)
     for seating in itertools.permutations(people, places):
@@ -43,37 +44,35 @@ def plan_seating(people):
     return most_happiness
 
 
-def find_happiest_seating_plan(filename):
+def find_happiest_seating_plan(filename: str) -> int:
     people = load_seating(filename)
     for person in people:
         print(person, people[person])
     return plan_seating(people)
 
 
-def add_me(people):
-    me = 'Me'
-    people[me] = {}
+def add_new_person(people: Dict[str, Dict[str, int]], new_person: str) -> None:
+    people[new_person] = {}
     for person in people:
-        people[me][person] = 0
-        people[person][me] = 0
-    return people
+        people[new_person][person] = 0
+        people[person][new_person] = 0
 
 
-def find_happiest_seating_plan_with_me(filename):
+def find_happiest_seating_plan_with_me(filename: str) -> int:
     people = load_seating(filename)
-    people = add_me(people)
+    add_new_person(people, 'Me')
     for person in people:
         print(person, people[person])
     return plan_seating(people)
 
 
-def test_plan_seating(filename, expected_happiness):
+def test_plan_seating(filename: str, expected_happiness: int) -> None:
     most_happiness = find_happiest_seating_plan(filename)
     assert most_happiness == expected_happiness, \
         'Expected happiness of {0} for {1} but was {2}!'.format(expected_happiness, filename, most_happiness)
 
 
-def main():
+def main() -> None:
     test_plan_seating('test13a.txt', 330)
     most_happiness = find_happiest_seating_plan('input13.txt')
     print('Day 13, Step 1 most happiness could be {0}'.format(most_happiness))
