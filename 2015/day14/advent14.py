b0e60913b72065024549357f0c815c06349c9fbc
John Chase <ratteal@gmail.com>
2020-01-25 16:34:33 +0000

    Review, make better use of function parameter and return types.

--- 2015/day14/advent14.py
+++ 2015/day14/advent14.py
@@ -1,9 +1,10 @@
+from typing import List
 import re
 
 
 class Reindeer:
 
-    def __init__(self, name, speed, duration, rest):
+    def __init__(self, name: str, speed: int, duration: int, rest: int) -> None:
         self.name = name
         self.speed = speed
         self.duration = duration
@@ -14,12 +15,7 @@ class Reindeer:
         self.points = None
 
     @classmethod
-    def from_line(cls, line):
-        name, speed, duration, rest = cls.decode_line(line)
-        return Reindeer(name, speed, duration, rest)
-
-    @staticmethod
-    def decode_line(line):
+    def from_line(cls, line: str):
         regex = r'(?P<name>\S+) can fly (?P<speed>\d+) km\/s for (?P<duration>\d+) seconds, ' \
                 r'but then must rest for (?P<rest>\d+) seconds\.'
         matches = re.match(regex, line)
@@ -27,15 +23,15 @@ class Reindeer:
         speed = int(matches.group('speed'))
         duration = int(matches.group('duration'))
         rest = int(matches.group('rest'))
-        return name, speed, duration, rest
+        return Reindeer(name, speed, duration, rest)
 
-    def into_the_starting_gate(self):
+    def into_the_starting_gate(self) -> None:
         self.distance = 0
         self.flying = 0
         self.resting = 0
         self.points = 0
 
-    def race(self):
+    def race(self) -> None:
         if self.resting > 0:
             self.resting -= 1
         else:
@@ -46,14 +42,14 @@ class Reindeer:
                 self.resting = self.rest
         print(self.name, self.distance, self.flying, self.resting)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Reindeer(Name: {0}, Speed: {1} km/s, Duration: {2}, Rest: {3})"\
             .format(self.name, self.speed, self.duration, self.rest)
 
 
 class Racer:
 
-    def __init__(self, duration):
+    def __init__(self, duration: int) -> None:
         self.duration = duration
 
     @staticmethod
@@ -62,14 +58,14 @@ class Racer:
             a_reindeer.into_the_starting_gate()
 
     @staticmethod
-    def __winning_distance(reindeer):
+    def __winning_distance(reindeer: List[Reindeer]) -> int:
         winning_distance = 0
         for a_reindeer in reindeer:
             if a_reindeer.distance > winning_distance:
                 winning_distance = a_reindeer.distance
         return winning_distance
 
-    def run_distance_race(self, reindeer):
+    def run_distance_race(self, reindeer: List[Reindeer]) -> int:
         self.__load_the_starting_gate(reindeer)
         for i in range(self.duration):
             for a_reindeer in reindeer:
@@ -77,7 +73,7 @@ class Racer:
         return self.__winning_distance(reindeer)
 
     @staticmethod
-    def __award_points(reindeer):
+    def __award_points(reindeer: List[Reindeer]) -> None:
         leaders_distance = 0
         for a_reindeer in reindeer:
             if a_reindeer.distance >= leaders_distance:
@@ -87,14 +83,14 @@ class Racer:
                 a_reindeer.points += 1
 
     @staticmethod
-    def __winning_points(reindeer):
+    def __winning_points(reindeer: List[Reindeer]) -> int:
         winning_points = 0
         for a_reindeer in reindeer:
             if a_reindeer.points > winning_points:
                 winning_points = a_reindeer.points
         return winning_points
 
-    def run_points_race(self, reindeer):
+    def run_points_race(self, reindeer: List[Reindeer]) -> int:
         self.__load_the_starting_gate(reindeer)
         for i in range(self.duration):
             for a_reindeer in reindeer:
@@ -103,7 +99,7 @@ class Racer:
         return self.__winning_points(reindeer)
 
 
-def load_reindeer(filename):
+def load_reindeer(filename: str) -> List[Reindeer]:
     reindeer = []
     with open(filename) as fh:
         for line in fh:
@@ -111,33 +107,33 @@ def load_reindeer(filename):
     return reindeer
 
 
-def find_winning_distance(filename, race_duration):
+def find_winning_distance(filename: str, race_duration: int) -> int:
     reindeer = load_reindeer(filename)
     for a_reindeer in reindeer:
         print(a_reindeer)
     return Racer(race_duration).run_distance_race(reindeer)
 
 
-def find_winning_points(filename, race_duration):
+def find_winning_points(filename: str, race_duration: int) -> int:
     reindeer = load_reindeer(filename)
     for a_reindeer in reindeer:
         print(a_reindeer)
     return Racer(race_duration).run_points_race(reindeer)
 
 
-def test_winning_distance(filename, race_duration, expected_distance):
+def test_winning_distance(filename: str, race_duration: int, expected_distance: int) -> None:
     distance = find_winning_distance(filename, race_duration)
     assert distance == expected_distance, \
         'Expected to find winning distance of {0} km but was {1}!'.format(expected_distance, distance)
 
 
-def test_winning_points(filename, race_duration, expected_points):
+def test_winning_points(filename: str, race_duration: int, expected_points: int) -> None:
     points = find_winning_points(filename, race_duration)
     assert points == expected_points, \
         'Expected to find winning points to be {0} but was {1}!'.format(expected_points, points)
 
 
-def main():
+def main() -> None:
     test_winning_distance('test14a.txt', 1000, 1120)
     race_duration = 2503
     winning_distance = find_winning_distance('input14.txt', race_duration)
