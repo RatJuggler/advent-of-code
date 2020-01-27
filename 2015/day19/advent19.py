8474355a4e68df63b9aab57e2a7be9f6cf241e71
John Chase <ratteal@gmail.com>
2020-01-27 18:05:30 +0000

    Load replacements and molecule from a file.

--- 2015/day19/advent19.py
+++ 2015/day19/advent19.py
@@ -1,4 +1,35 @@
-def find_distinct_molecules(filename):
+from typing import List, Tuple
+import re
+
+
+def from_line(line: str) -> [Tuple[str, str], str]:
+    regex = r'(?P<from_atom>\S+) => (?P<to_atom>\S+)|(?P<molecule>\S+)'
+    matches = re.finditer(regex, line)
+    molecule = ''
+    replacement = ()
+    for match in matches:
+        if match.group('molecule'):
+            molecule = match.group('molecule')
+        else:
+            from_atom = match.group('from_atom')
+            to_atom = match.group('to_atom')
+            replacement = (from_atom, to_atom)
+    return replacement, molecule
+
+
+def load_replacements_and_molecule(filename: str) -> [List[Tuple[str, str]], str]:
+    replacements = []
+    with open(filename) as fh:
+        for line in fh:
+            replacement, molecule = from_line(line)
+            if replacement is not ():
+                replacements.append(replacement)
+    return replacements, molecule
+
+
+def find_distinct_molecules(filename: str) -> int:
+    replacements, molecule = load_replacements_and_molecule(filename)
+    print(replacements, molecule)
     return 0
 
 
