f3d5ad3c391c44deef1147db05fe39829ade95c1
John Chase <ratteal@gmail.com>
2020-01-27 12:22:51 +0000

    Test for Step 2.

--- 2015/day17/advent17.py
+++ 2015/day17/advent17.py
@@ -23,13 +23,30 @@ def test_container_combination(to_store: int, filename: str, expected_combinatio
     combinations = container_combinations(to_store, containers)
     assert combinations == expected_combinations, \
         'Expected to find {0} combinations of containers to store {1}l but found {2}!'\
-            .format(expected_combinations, to_store, combinations)
+        .format(expected_combinations, to_store, combinations)
+
+
+def minimum_container_combinations(to_store: int, containers: List[int]) -> [int, int]:
+    return 0, 0
+
+
+def test_minimum_container_combination(to_store: int, filename: str,
+                                       expected_minimum_containers: int, expected_combinations: int) -> None:
+    containers = load_containers(filename)
+    minimum_containers, combinations = minimum_container_combinations(to_store, containers)
+    assert minimum_containers == expected_minimum_containers, \
+        'Expected to find minimum of {0} containers needed to store {1}l but found {2}!'\
+        .format(expected_minimum_containers, to_store, minimum_containers)
+    assert combinations == expected_combinations, \
+        'Expected to find {0} combinations of the minimum number of containers to store {1}l but found {2}!'\
+        .format(expected_combinations, to_store, combinations)
 
 
 def main() -> None:
     test_container_combination(25, 'test17a.txt', 4)
     containers = load_containers('input17.txt')
     print('Day 17, Step 1 container combinations found {0}.'.format(container_combinations(150, containers)))
+    test_minimum_container_combination(25, 'test17a.txt', 2, 3)
 
 
 if __name__ == '__main__':
