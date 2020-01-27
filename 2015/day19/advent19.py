2c05585e6f88cee8000e2f9d787e92bc99d75fe6
John Chase <ratteal@gmail.com>
2020-01-27 18:46:49 +0000

    Ensure test for bug fix is passing.

--- 2015/day19/advent19.py
+++ 2015/day19/advent19.py
@@ -50,7 +50,7 @@ def test_find_distinct_molecules(filename: str, expected_distinct_molecules: int
 def main() -> None:
     test_find_distinct_molecules('test19a.txt', 4)
     test_find_distinct_molecules('test19b.txt', 7)
-    test_find_distinct_molecules('test19c.txt', 7)
+    test_find_distinct_molecules('test19c.txt', 2)
     distinct_molecules = find_distinct_molecules('input19.txt')
     print('Day 19, Step 1 found {0} distinct new molecules.'.format(distinct_molecules))
 
