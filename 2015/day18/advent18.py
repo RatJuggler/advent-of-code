92a9a718d98a9ebebf2ba0684436b0c88b54c22a
John Chase <ratteal@gmail.com>
2020-01-27 16:33:24 +0000

    Step 2 solution with mechanism to set stuck lights.

--- 2015/day18/advent18.py
+++ 2015/day18/advent18.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import List
 
 
@@ -6,14 +7,7 @@ class LightGrid:
     def __init__(self, grid: List[List[int]]) -> None:
         self.grid = grid
         self.grid_size = len(grid)
-
-    @classmethod
-    def from_empty(cls, grid_size: int):
-        grid = []
-        for y in range(grid_size):
-            grid_row = [0] * grid_size
-            grid.append(grid_row)
-        return LightGrid(grid)
+        self.stuck_lights = []
 
     @classmethod
     def from_file(cls, filename: str):
@@ -26,8 +20,13 @@ class LightGrid:
                 grid.append(grid_row)
         return LightGrid(grid)
 
+    def set_stuck_light(self, x: int, y: int, stuck_state: int) -> None:
+        self.grid[y][x] = stuck_state
+        self.stuck_lights.append((x, y))
+
     def set_light(self, x: int, y: int, new_light) -> None:
-        self.grid[y][x] = new_light
+        if (y, x) not in self.stuck_lights:
+            self.grid[y][x] = new_light
 
     def get_light(self, x: int, y: int) -> int:
         if x < 0 or y < 0:
@@ -37,15 +36,15 @@ class LightGrid:
         return self.grid[y][x]
 
     def next_light_state(self, x: int, y: int) -> int:
-        current_light = 0
+        surrounding_lights = 0
         for sy in range(y - 1, y + 2):
             for sx in range(x - 1, x + 2):
                 if sx != x or sy != y:
-                    current_light += self.get_light(sx, sy)
+                    surrounding_lights += self.get_light(sx, sy)
         if self.get_light(x, y) == 1:
-            return 1 if current_light in (2, 3) else 0
+            return 1 if surrounding_lights in (2, 3) else 0
         else:
-            return 1 if current_light == 3 else 0
+            return 1 if surrounding_lights == 3 else 0
 
     def count_lights_on(self) -> int:
         lights_lit = 0
@@ -55,10 +54,15 @@ class LightGrid:
         return lights_lit
 
 
-def animate_light_grid(filename: str, steps: int) -> int:
+def animate_light_grid(filename: str, steps: int, add_stuck_lights: bool) -> int:
     light_grid = LightGrid.from_file(filename)
+    if add_stuck_lights:
+        light_grid.set_stuck_light(0, 0, 1)
+        light_grid.set_stuck_light(light_grid.grid_size - 1, 0, 1)
+        light_grid.set_stuck_light(0, light_grid.grid_size - 1, 1)
+        light_grid.set_stuck_light(light_grid.grid_size - 1, light_grid.grid_size - 1, 1)
     for i in range(steps):
-        new_light_grid = LightGrid.from_empty(light_grid.grid_size)
+        new_light_grid = deepcopy(light_grid)
         for y in range(light_grid.grid_size):
             for x in range(light_grid.grid_size):
                 new_light_grid.set_light(x, y, light_grid.next_light_state(x, y))
@@ -66,17 +70,20 @@ def animate_light_grid(filename: str, steps: int) -> int:
     return light_grid.count_lights_on()
 
 
-def test_animate_light_grid(filename: str, steps: int, expected_lights_on: int) -> None:
-    lights_on = animate_light_grid(filename, steps)
+def test_animate_light_grid(filename: str, add_stuck_lights: bool, steps: int, expected_lights_on: int) -> None:
+    lights_on = animate_light_grid(filename, steps, add_stuck_lights)
     assert lights_on == expected_lights_on, \
         'Expected to find {0} lights on after {1} steps but was {2}!'.format(expected_lights_on, steps, lights_on)
 
 
 def main() -> None:
-    test_animate_light_grid('test18a.txt', 4, 4)
+    test_animate_light_grid('test18a.txt', False, 4, 4)
     steps = 100
-    light_count = animate_light_grid('input18.txt', steps)
+    light_count = animate_light_grid('input18.txt', steps, False)
     print('Day 18, Step 1 after {0} steps there are {1} lights on.'.format(steps, light_count))
+    test_animate_light_grid('test18a.txt', True, 5, 17)
+    light_count = animate_light_grid('input18.txt', steps, True)
+    print('Day 18, Step 2 with stuck lights after {0} steps there are {1} lights on.'.format(steps, light_count))
 
 
 if __name__ == '__main__':
