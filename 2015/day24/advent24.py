092fddc74f1d3941cf5cc151ba7d0de976f477f0
John Chase <ratteal@gmail.com>
2020-01-30 18:17:48 +0000

    Find minimum Group 1 loading and quantum entanglement.

--- 2015/day24/advent24.py
+++ 2015/day24/advent24.py
@@ -32,17 +32,19 @@ def determine_sleigh_loading_combinations(packages: List[int]) -> List[List[List
     return sleigh_loadings
 
 
-def determine_quantum_entanglements(sleigh_loadings: List[List[List[int]]]) -> List[int]:
+def determine_quantum_entanglements(sleigh_loadings: List[List[List[int]]], minimum_group1: int) -> List[int]:
     quantum_entanglements = []
     for loading in sleigh_loadings:
-        quantum_entanglements.append(calculate_quantum_entanglement(loading[0]))
+        if len(loading[0]) == minimum_group1:
+            quantum_entanglements.append(calculate_quantum_entanglement(loading[0]))
     return quantum_entanglements
 
 
 def find_best_sleigh_loading(filename: str) -> int:
     packages = load_package_weights(filename)
     sleigh_loadings = determine_sleigh_loading_combinations(packages)
-    quantum_entanglements = determine_quantum_entanglements(sleigh_loadings)
+    minimum_group1 = min([len(loadings[0]) for loadings in sleigh_loadings])
+    quantum_entanglements = determine_quantum_entanglements(sleigh_loadings, minimum_group1)
     return min(quantum_entanglements)
 
 
