e22390fd4126a761e66412e1900e0aba1eb65226
John Chase <ratteal@gmail.com>
2020-01-31 10:54:43 +0000

    Show order code sheet cells are visisted.

--- 2015/day25/advent25.py
+++ 2015/day25/advent25.py
@@ -6,15 +6,24 @@ def build_blank_code_sheet(size: int) -> List[List[int]]:
     for row in range(1, size + 1):
         sheet_row = []
         for column in range(1, size - row + 2):
-            sheet_row.append(column)
+            sheet_row.append(0)
         code_sheet.append(sheet_row)
     return code_sheet
 
 
 def test_code_sheet_layout(size: int) -> None:
     code_sheet = build_blank_code_sheet(size)
-    for row in code_sheet:
-        print(row)
+    for sheet_row in code_sheet:
+        print(sheet_row)
+    code_sequence = 1
+    for i in range(1, size + 1):
+        column = 0
+        for row in range(i, 0, -1):
+            code_sheet[row - 1][column] = code_sequence
+            code_sequence += 1
+            column += 1
+    for sheet_row in code_sheet:
+        print(sheet_row)
 
 
 def main() -> None:
