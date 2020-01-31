0d5fc8f22549088faa13bf993b638eb9d835fae6
John Chase <ratteal@gmail.com>
2020-01-31 13:00:37 +0000

    Step 1 solution but getting out of range.

--- 2015/day25/advent25.py
+++ 2015/day25/advent25.py
@@ -11,7 +11,8 @@ def build_blank_code_sheet(size: int) -> List[List[int]]:
     return code_sheet
 
 
-def calculate_codes(code_sheet: List[List[int]], start_code: int, next_code: Callable[[int], int]) -> None:
+def calculate_codes(size: int, start_code: int, next_code: Callable[[int], int]) -> List[List[int]]:
+    code_sheet = build_blank_code_sheet(size)
     code_sequence = start_code
     for i in range(1, len(code_sheet) + 1):
         column = 0
@@ -19,11 +20,11 @@ def calculate_codes(code_sheet: List[List[int]], start_code: int, next_code: Cal
             code_sheet[row - 1][column] = code_sequence
             code_sequence = next_code(code_sequence)
             column += 1
+    return code_sheet
 
 
 def test_code_sheet_layout(size: int) -> None:
-    code_sheet = build_blank_code_sheet(size)
-    calculate_codes(code_sheet, 1, lambda x: x + 1)
+    code_sheet = calculate_codes(size, 1, lambda x: x + 1)
     for sheet_row in code_sheet:
         print(sheet_row)
     assert code_sheet[3][1] == 12, 'Expect row 4, column 2 to be 12 but was {0}!'.format(code_sheet[3][1])
@@ -31,8 +32,7 @@ def test_code_sheet_layout(size: int) -> None:
 
 
 def test_code_sheet_calculation(size: int ) -> None:
-    code_sheet = build_blank_code_sheet(size)
-    calculate_codes(code_sheet, 20151125, lambda x: (x * 252533) % 33554393)
+    code_sheet = calculate_codes(size, 20151125, lambda x: (x * 252533) % 33554393)
     for sheet_row in code_sheet:
         print(sheet_row)
     assert code_sheet[3][1] == 32451966, 'Expect row 4, column 2 to be 32451966 but was {0}!'.format(code_sheet[3][1])
@@ -42,6 +42,8 @@ def test_code_sheet_calculation(size: int ) -> None:
 def main() -> None:
     test_code_sheet_layout(6)
     test_code_sheet_calculation(6)
+    code_sheet = calculate_codes(3019, 20151125, lambda x: (x * 252533) % 33554393)
+    print('Day 24, Step 1 code at row 3010, column 3019 is {0}'.format(code_sheet[3009][3018]))
 
 
 if __name__ == '__main__':
