e5e7edba2548909e4f56c9e4114014a4df3484b6
John Chase <ratteal@gmail.com>
2020-02-01 14:20:14 +0000

    Improve generation of visit tracker and clarify present calculation.

--- 2015/day20/advent20.py
+++ 2015/day20/advent20.py
@@ -14,7 +14,7 @@ def infinite_delivery_step1(find_presents: int) -> int:
 def infinite_delivery_step2(find_presents: int) -> int:
     house_number = 0
     house_presents = 0
-    visits = [0 for i in range(0, (find_presents // 11) + 1)]
+    visits = [0] * (find_presents // 11)
     while house_presents < find_presents:
         house_number += 1
         house_presents = 0
@@ -23,9 +23,9 @@ def infinite_delivery_step2(find_presents: int) -> int:
                 house_presents += elf * 11
                 visits[elf] += 1
         if visits[house_number] < 50:
-            house_presents += house_number * 11
+            house_presents += (house_number * 11)
             visits[house_number] += 1
-#        print(house_number, house_presents)
+        print(house_number, house_presents)
     return house_number
 
 
@@ -42,12 +42,12 @@ def test_infinite_delivery_step2(find_presents: int, expected_house: int) -> Non
 
 
 def main() -> None:
-    # test_infinite_delivery_step1(150, 8)
+    test_infinite_delivery_step1(150, 8)
     # house = infinite_delivery_step1(29000000)     # 665280
     # print('Day 20, Step 1 first house to receive 29000000 presents is {0}.'.format(house))
-    test_infinite_delivery_step2(160, 8)
-    house = infinite_delivery_step2(29000000)
-    print('Day 20, Step 2 first house to receive 29000000 presents is {0}.'.format(house))
+    # test_infinite_delivery_step2(160, 8)
+    # house = infinite_delivery_step2(29000000)
+    # print('Day 20, Step 2 first house to receive 29000000 presents is {0}.'.format(house))
 
 
 if __name__ == '__main__':
