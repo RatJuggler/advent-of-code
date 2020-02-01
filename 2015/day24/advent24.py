d23b6c8ee363c70d4339c6bebec27b909060f8fd
John Chase <ratteal@gmail.com>
2020-02-01 17:08:03 +0000

    Trying to reduce the effort but not making much difference.

--- 2015/day24/advent24.py
+++ 2015/day24/advent24.py
@@ -23,17 +23,25 @@ def determine_sleigh_loading_combinations(packages: List[int]) -> List[List[List
     group_sizes = range(1, len(packages) - 1)
     group_lengths = [lengths for lengths in itertools.combinations_with_replacement(group_sizes, 3)
                      if sum(lengths) == len(packages)]
+    max_group_length = max([max(group) for group in group_lengths])
+    group_sums = []
+    for i in range(1, max_group_length + 1):
+        print('Finding sums for group length {0}'.format(i))
+        group_sums.append(set([sum(group) for group in itertools.combinations(packages, i)]))
+    print(group_sums)
     for lengths in group_lengths:
         print(lengths)
-        for group1 in itertools.combinations(packages, lengths[0]):
-            group_sum = sum(group1)
-            group1_remainder = [package for package in packages if package not in group1]
-            for group2 in [group for group in itertools.combinations(group1_remainder, lengths[1])
+        for group_sum in group_sums[lengths[0] - 1].intersection(group_sums[lengths[1] - 1], group_sums[lengths[2] - 1]):
+            print(group_sum)
+            for group1 in [group for group in itertools.combinations(packages, lengths[0])
                            if sum(group) == group_sum]:
-                group3 = [package for package in packages if package not in group1 and package not in group2]
-                if sum(group3) == group_sum:
-                    sleigh_loadings.append([group1, group2, group3])
-                    print(group1, group2, group3)
+                group1_remainder = [package for package in packages if package not in group1]
+                for group2 in [group for group in itertools.combinations(group1_remainder, lengths[1])
+                               if sum(group) == group_sum]:
+                    group3 = [package for package in packages if package not in group1 and package not in group2]
+                    if sum(group3) == group_sum:
+                        sleigh_loadings.append([group1, group2, group3])
+                        print(group1, group2, group3)
     return sleigh_loadings
 
 
@@ -60,7 +68,7 @@ def test_find_best_sleigh_loading(filename: str, expected_quantum_entanglement: 
 
 
 def main() -> None:
-#    test_find_best_sleigh_loading('test24a.txt', 99)
+    test_find_best_sleigh_loading('test24a.txt', 99)
     quantum_entanglement = find_best_sleigh_loading('input24.txt')
     print('Day 24, Step 1 best quantum entanglement for Group 1 packages is {0}.'.format(quantum_entanglement))
 
