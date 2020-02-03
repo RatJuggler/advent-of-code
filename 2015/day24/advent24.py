fb28204cc02d8b295926b8b05db20cb76a18afc1
John Chase <ratteal@gmail.com>
2020-02-03 17:24:07 +0000

    Reduce the group size combinations checked.

--- 2015/day24/advent24.py
+++ 2015/day24/advent24.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import itertools
 
 
@@ -10,36 +10,59 @@ def load_package_weights(filename: str) -> List[int]:
     return packages
 
 
-def calculate_quantum_entanglement(packages: List[int]) -> int:
+def group_overlap(group: Tuple[int], previous_group: Tuple[int]):
+    for package in group:
+        if package in previous_group:
+            return True
+    return False
+
+
+def groups_overlap(group: Tuple[int], previous_group1: Tuple[int], previous_group2: Tuple[int]):
+    for package in group:
+        if package in previous_group1 or package in previous_group2:
+            return True
+    return False
+
+
+def determine_sleigh_loading_combinations(packages: List[int]) -> List[List[Tuple[int]]]:
+    sleigh_loadings = []
+    # We must have three groups of packages with a minimum of one package per group.
+    max_group_size = len(packages) - 2
+    # This gives us all the possible combinations of group sizes.
+    group_sizes = [lengths for lengths in itertools.combinations_with_replacement(range(1, max_group_size + 1), 3)
+                   if sum(lengths) == len(packages)]
+    # Each group must sum to a third of the total packages.
+    group_sum = sum(packages) // 3
+    print('Group sizes from 1-{0} must sum to {1}'.format(max_group_size, group_sum))
+    groups = []
+    for group_size in range(1, max_group_size + 1):
+        # Every combination of presents which sums to the total.
+        group = [group for group in itertools.combinations(packages, group_size)
+                 if sum(group) == group_sum]
+        groups.append(group)
+        if not group:
+            group_sizes = [sizes for sizes in group_sizes if group_size not in sizes]
+    for sizes in group_sizes:
+        for size in sizes:
+            print(size, 'x', len(groups[size - 1]), ' ', end='')
+        print('')
+        for group1 in [group for group in groups[sizes[0] - 1]]:
+            for group2 in [group for group in groups[sizes[1] - 1]
+                           if not group_overlap(group, group1)]:
+                for group3 in [group for group in groups[sizes[2] - 1]
+                               if not groups_overlap(group, group1, group2)]:
+                    sleigh_loadings.append([group1, group2, group3])
+    return sleigh_loadings
+
+
+def calculate_quantum_entanglement(packages: Tuple[int]) -> int:
     quantum_entanglement = 1
     for package in packages:
         quantum_entanglement *= package
     return quantum_entanglement
 
 
-def determine_sleigh_loading_combinations(packages: List[int]) -> List[List[List[int]]]:
-    sleigh_loadings = []
-    # We must have three groups of packages with a minimum of one package per group.
-    group_sizes = range(1, len(packages) - 1)
-    group_lengths = [lengths for lengths in itertools.combinations_with_replacement(group_sizes, 3)
-                     if sum(lengths) == len(packages)]
-    group_sum = sum(packages) // len(packages)
-    print('Each group must sum to {0}'.format(group_sum))
-    for lengths in group_lengths:
-        print(lengths)
-        for group1 in [group for group in itertools.combinations(packages, lengths[0])
-                       if sum(group) == group_sum]:
-            group1_remainder = [package for package in packages if package not in group1]
-            for group2 in [group for group in itertools.combinations(group1_remainder, lengths[1])
-                           if sum(group) == group_sum]:
-                group3 = [package for package in packages if package not in group1 and package not in group2]
-                if sum(group3) == group_sum:
-                    sleigh_loadings.append([group1, group2, group3])
-                    print(group1, group2, group3)
-    return sleigh_loadings
-
-
-def determine_quantum_entanglements(sleigh_loadings: List[List[List[int]]], minimum_group1: int) -> List[int]:
+def determine_quantum_entanglements(sleigh_loadings: List[List[Tuple[int]]], minimum_group1: int) -> List[int]:
     quantum_entanglements = []
     for loading in sleigh_loadings:
         if len(loading[0]) == minimum_group1:
