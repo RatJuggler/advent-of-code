c9d6c39b09f54094ef764edb24d6c7ab1da3ae1b
John Chase <ratteal@gmail.com>
2020-02-05 09:22:26 +0000

    Remove duplicated code.

--- 2015/day17/advent17.py
+++ 2015/day17/advent17.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import itertools
 
 
@@ -10,29 +10,26 @@ def load_containers(filename: str) -> List[int]:
     return containers
 
 
-def container_combinations(to_store: int, containers: List[int]) -> int:
+def container_combinations(to_store: int, containers: List[int]) -> List[Tuple[int]]:
     combinations = [seq for i in range(len(containers), 0, -1)
                     for seq in itertools.combinations(containers, i)
                     if sum(seq) == to_store]
     print(combinations)
-    return len(combinations)
+    return combinations
 
 
 def test_container_combination(to_store: int, filename: str, expected_combinations: int) -> None:
     containers = load_containers(filename)
-    combinations = container_combinations(to_store, containers)
+    combinations = len(container_combinations(to_store, containers))
     assert combinations == expected_combinations, \
         'Expected to find {0} combinations of containers to store {1}l but found {2}!'\
         .format(expected_combinations, to_store, combinations)
 
 
 def minimum_container_combinations(to_store: int, containers: List[int]) -> [int, int]:
-    combinations = [seq for i in range(len(containers), 0, -1)
-                    for seq in itertools.combinations(containers, i)
-                    if sum(seq) == to_store]
+    combinations = container_combinations(to_store, containers)
     minimum_containers = min([len(combination) for combination in combinations])
     minimum_combinations = len([combination for combination in combinations if len(combination) == minimum_containers])
-    print(combinations)
     return minimum_containers, minimum_combinations
 
 
@@ -51,7 +48,7 @@ def test_minimum_container_combination(to_store: int, filename: str,
 def main() -> None:
     test_container_combination(25, 'test17a.txt', 4)
     containers = load_containers('input17.txt')
-    print('Day 17, Step 1 container combinations found {0}.'.format(container_combinations(150, containers)))
+    print('Day 17, Step 1 container combinations found {0}.'.format(len(container_combinations(150, containers))))
     test_minimum_container_combination(25, 'test17a.txt', 2, 3)
     minimum_containers, minimum_combinations = minimum_container_combinations(150, containers)
     print('Day 17, Step 2 minimum of {0} containers with {1} combinations found.'
