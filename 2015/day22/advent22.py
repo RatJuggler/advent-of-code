6ac2685911188c3cc99ce688b0df8253539e2980
John Chase <ratteal@gmail.com>
2020-02-05 18:42:30 +0000

    Somewhat complicated but test is passing.

--- 2015/day22/advent22.py
+++ 2015/day22/advent22.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 class Spell:
 
-    def __init__(self, name: str, cost: int, duration: int, effects: Dict[str: int]) -> None:
+    def __init__(self, name: str, cost: int, duration: int, effects: Dict[str, int]) -> None:
         self.name = name
         self.cost = cost
         self.duration = duration
@@ -13,24 +13,63 @@ class Spell:
 
 class Character(ABC):
 
-    def __init__(self, name: str, hp: int, armour: int = 0) -> None:
+    def __init__(self, name: str, hp: int) -> None:
         self.name = name
         self.hp = hp
-        self.armour = armour
+        self.armour = 0
+        self.damage = 0
 
     @abstractmethod
-    def turn(self, light: int) -> int:
+    def turn(self) -> None:
+        pass
+
+    @abstractmethod
+    def apply_current_effects(self) -> None:
         pass
 
     def __repr__(self) -> str:
-        return '{0}(HP:{1}, Armour:{2})'.format(self.name, self.hp, self.armour)
+        return '{0} - HP:{1}, Armour:{2}'.format(self.name, self.hp, self.armour)
 
 
 class Mage(Character):
 
-    def __init__(self, name: str, hp: int, mana: int) -> None:
+    def __init__(self, name: str, hp: int, mana: int, spell_book: List[Spell], spells_to_cast: List[int]) -> None:
         super().__init__(name, hp)
         self.mana = mana
+        self.spell_book = spell_book
+        self.spells_to_cast = spells_to_cast
+        self.current_cast = 0
+        self.current_effects = []
+        self.damage = 0
+
+    def turn(self) -> None:
+        self.apply_current_effects()
+        cast_spell = self.spell_book[self.spells_to_cast[self.current_cast]]
+        print('{0} casts {1}!'.format(self.name, cast_spell.name))
+        self.mana -= cast_spell.cost
+        if cast_spell.duration > 0:
+            self.current_effects.append([cast_spell.duration, cast_spell])
+        else:
+            print('{0} applies {1} instantly!'.format(cast_spell.name, cast_spell.effects))
+            self.apply_effects(cast_spell.effects)
+        self.current_cast += 1
+
+    def apply_effects(self, spell_effects: Dict[str, int]) -> None:
+        for attribute in spell_effects:
+            setattr(self, attribute, getattr(self, attribute) + spell_effects[attribute])
+
+    def apply_current_effects(self) -> None:
+        self.damage = 0
+        self.armour = 0
+        for effects in self.current_effects:
+            effects[0] -= 1
+            spell = effects[1]
+            self.apply_effects(spell.effects)
+            print('{0} applies {1}, duration is now {2}!'.format(spell.name, spell.effects, effects[0]))
+        self.current_effects = [effects for effects in self.current_effects if effects[0] > 0]
+
+    def __repr__(self) -> str:
+        return super().__repr__() + ', Mana: {0}'.format(self.mana)
 
 
 class Fighter(Character):
@@ -39,29 +78,51 @@ class Fighter(Character):
         super().__init__(name, hp)
         self.damage = damage
 
+    def turn(self) -> None:
+        print('{0} attacks with {1} damage!'.format(self.name, self.damage))
 
-def spells() -> List[Spell]:
-    return [Spell('Magic Missile', 53, 0, {'damage': -4}),
-            Spell('Drain', 73, 0, {'damage': -2, 'hp': 2}),
+    def apply_current_effects(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return super().__repr__() + ', Damage: {0}'.format(self.damage)
+
+
+def spells_available() -> List[Spell]:
+    return [Spell('Magic Missile', 53, 0, {'damage': 4}),
+            Spell('Drain', 73, 0, {'damage': 2, 'hp': 2}),
             Spell('Shield', 113, 6, {'armour': 7}),
-            Spell('Poison', 173, 6, {'hp': -3}),
+            Spell('Poison', 173, 6, {'damage': 3}),
             Spell('Recharge', 229, 5, {'mana': 101})]
 
 
 def fight(hero: Character, boss: Character) -> int:
     print('{0} vs {1}'.format(hero, boss))
-    hero_damage = 1 if boss.armour > hero.damage else hero.damage - boss.armour + 1
-    boss_damage = 1 if hero.armour > boss.damage else boss.damage - hero.armour + 1
-    while hero.hp > 0 and boss.hp > 0:
-        hero.hp -= boss_damage
+    while True:
+        print('-- Hero turn --')
+        hero.turn()
+        hero_damage = 1 if boss.armour > hero.damage else hero.damage - boss.armour
         boss.hp -= hero_damage
+        print('-- Boss turn --')
+        hero.apply_current_effects()
+        hero_damage = 1 if boss.armour > hero.damage else hero.damage - boss.armour
+        boss.hp -= hero_damage
+        print('{0} vs {1}'.format(hero, boss))
+        if boss.hp <= 0:
+            break
+        boss.turn()
+        boss_damage = 1 if hero.armour > boss.damage else boss.damage - hero.armour
+        hero.hp -= boss_damage
+        if hero.hp <= 0:
+            break
+        print('{0} vs {1}'.format(hero, boss))
     winner = 1 if boss.hp <= 0 else 2
     print('{0} wins!'.format(hero.name if winner == 1 else boss.name))
     return winner
 
 
 def test_fight() -> None:
-    test_hero = Mage('Hero', 10, 250)
+    test_hero = Mage('Hero', 10, 250, spells_available(), [3, 0])
     test_boss = Fighter('Boss', 13, 8)
     winner = fight(test_hero, test_boss)
     assert winner == 1, 'Expected hero to win!'
