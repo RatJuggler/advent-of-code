a49df057bf1b821ad0818c78924283369864f484
John Chase <ratteal@gmail.com>
2020-02-05 17:03:02 +0000

    Explore options for holding spells and character details.

--- 2015/day22/advent22.py
+++ 2015/day22/advent22.py
@@ -1,11 +1,51 @@
-class Character:
+from abc import ABC, abstractmethod
+from typing import Dict, List
 
-    def __init__(self, name: str, hp: int, mana: int, damage: int = 0, armour: int = 0) -> None:
+
+class Spell:
+
+    def __init__(self, name: str, cost: int, duration: int, effects: Dict[str: int]) -> None:
+        self.name = name
+        self.cost = cost
+        self.duration = duration
+        self.effects = effects
+
+
+class Character(ABC):
+
+    def __init__(self, name: str, hp: int, armour: int = 0) -> None:
         self.name = name
         self.hp = hp
-        self.mana = mana
-        self.damage = damage
         self.armour = armour
+
+    @abstractmethod
+    def turn(self, light: int) -> int:
+        pass
+
+    def __repr__(self) -> str:
+        return '{0}(HP:{1}, Armour:{2})'.format(self.name, self.hp, self.armour)
+
+
+class Mage(Character):
+
+    def __init__(self, name: str, hp: int, mana: int) -> None:
+        super().__init__(name, hp)
+        self.mana = mana
+
+
+class Fighter(Character):
+
+    def __init__(self, name: str, hp: int, damage: int) -> None:
+        super().__init__(name, hp)
+        self.damage = damage
+
+
+def spells() -> List[Spell]:
+    return [Spell('Magic Missile', 53, 0, {'damage': -4}),
+            Spell('Drain', 73, 0, {'damage': -2, 'hp': 2}),
+            Spell('Shield', 113, 6, {'armour': 7}),
+            Spell('Poison', 173, 6, {'hp': -3}),
+            Spell('Recharge', 229, 5, {'mana': 101})]
 
 
 def fight(hero: Character, boss: Character) -> int:
@@ -21,8 +61,8 @@ def fight(hero: Character, boss: Character) -> int:
 
 
 def test_fight() -> None:
-    test_hero = Character('Hero', 10, 250)
-    test_boss = Character('Boss', 13, 0, 8)
+    test_hero = Mage('Hero', 10, 250)
+    test_boss = Fighter('Boss', 13, 8)
     winner = fight(test_hero, test_boss)
     assert winner == 1, 'Expected hero to win!'
 
