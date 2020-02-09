4ef65bc9e45dd8e6f36d7309bea50a9a02959b7a
John Chase <ratteal@gmail.com>
2020-02-09 23:00:44 +0000

    Part 1 solution.

---
+++ 2016/day1/Advent1.java
@@ -0,0 +1,79 @@
+package day1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+public class Advent1 {
+
+    private static String readInputFile(String filepath) throws IOException {
+        Path filePath = Paths.get(filepath);
+        return Files.readString(filePath);
+    }
+
+    private static int blocks_away(String directions) {
+        int x = 0, y = 0;
+        char heading = 'N';
+        StringTokenizer tokenizer = new StringTokenizer(directions, ", ");
+        while (tokenizer.hasMoreElements()) {
+            String direction = tokenizer.nextToken();
+            char turn = direction.charAt(0);
+            int distance = Integer.parseInt(direction.substring(1));
+            if (heading == 'N') {
+                if (turn == 'R') {
+                    heading = 'E';
+                    x += distance;
+                } else {
+                    heading = 'W';
+                    x -= distance;
+                }
+            } else if (heading == 'E') {
+                if (turn == 'R') {
+                    heading = 'S';
+                    y -= distance;
+                } else {
+                    heading = 'N';
+                    y += distance;
+                }
+            } else if (heading == 'S') {
+                if (turn == 'R') {
+                    heading = 'W';
+                    x -= distance;
+                } else {
+                    heading = 'E';
+                    x += distance;
+                }
+            } else if (heading == 'W') {
+                if (turn == 'R') {
+                    heading = 'N';
+                    y += distance;
+                } else {
+                    heading = 'S';
+                    y -= distance;
+                }
+            } else {
+                throw new IllegalStateException(String.format("Impossible heading found %s!", heading));
+            }
+            System.out.println(String.format("Turn: %s, Distance: %d -> (%d, %d)", turn, distance, x, y));
+        }
+        return Math.abs(x) + Math.abs(y);
+    }
+
+    private static void test_blocks_away(String directions, int expected_distance) {
+        int distance = blocks_away(directions);
+        assert distance == expected_distance:
+                String.format("Expected a distance of %d but was %d!", expected_distance, distance);
+    }
+
+    public static void main(String[] args) throws IOException {
+        test_blocks_away("R2, L3", 5);
+        test_blocks_away("R2, R2, R2", 2);
+        test_blocks_away("R5, L5, R5, R3", 12);
+        String directions = readInputFile("2016/day1/input1.txt");
+        int distance = blocks_away(directions);
+        System.out.println(String.format("Day 1, Part 1 the Easter Bunny HQ is %d blocks away.", distance));
+    }
+
+}
