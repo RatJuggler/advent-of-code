ef0f928efd72b6ad0b30d557554db69a54b28b48
John Chase <ratteal@gmail.com>
2020-02-10 09:37:19 +0000

    Create a position class to move about.

--- 2016/day1/Advent1.java
+++ 2016/day1/Advent1.java
@@ -6,64 +6,86 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.StringTokenizer;
 
-public class Advent1 {
+final class Position {
 
-    private static String readInputFile(final String filepath) throws IOException {
-        Path filePath = Paths.get(filepath);
+    // Always start at origin facing North.
+    private int x = 0;
+    private int y = 0;
+    private char heading = 'N';
+
+    Position() {}
+
+    void move(final char turn, final int distance) {
+        switch (heading) {
+            case 'N':
+                if (turn == 'R') {
+                    heading = 'E';
+                    x += distance;
+                } else {
+                    heading = 'W';
+                    x -= distance;
+                }
+                break;
+            case 'E':
+                if (turn == 'R') {
+                    heading = 'S';
+                    y -= distance;
+                } else {
+                    heading = 'N';
+                    y += distance;
+                }
+                break;
+            case 'S':
+                if (turn == 'R') {
+                    heading = 'W';
+                    x -= distance;
+                } else {
+                    heading = 'E';
+                    x += distance;
+                }
+                break;
+            case 'W':
+                if (turn == 'R') {
+                    heading = 'N';
+                    y += distance;
+                } else {
+                    heading = 'S';
+                    y -= distance;
+                }
+                break;
+            default:
+                throw new IllegalStateException(String.format("Impossible heading found %s!", heading));
+        }
+    }
+
+    int getDistance() {
+        return Math.abs(this.x) + Math.abs(this.y);
+    }
+
+    @Override
+    public String toString() {
+        return "Position(x:" + x + ", y:" + y + ", heading:" + heading + ')';
+    }
+}
+
+final class Advent1 {
+
+    private static String readInputFile() throws IOException {
+        Path filePath = Paths.get("2016/day1/input1.txt");
         return Files.readString(filePath);
     }
 
     private static int blocks_away(final String directions) {
-        int x = 0, y = 0;
-        char heading = 'N';
+        Position position = new Position();
         StringTokenizer tokenizer = new StringTokenizer(directions, ", ");
         while (tokenizer.hasMoreElements()) {
             String direction = tokenizer.nextToken();
             char turn = direction.charAt(0);
             int distance = Integer.parseInt(direction.substring(1));
-            switch (heading) {
-                case 'N':
-                    if (turn == 'R') {
-                        heading = 'E';
-                        x += distance;
-                    } else {
-                        heading = 'W';
-                        x -= distance;
-                    }
-                    break;
-                case 'E':
-                    if (turn == 'R') {
-                        heading = 'S';
-                        y -= distance;
-                    } else {
-                        heading = 'N';
-                        y += distance;
-                    }
-                    break;
-                case 'S':
-                    if (turn == 'R') {
-                        heading = 'W';
-                        x -= distance;
-                    } else {
-                        heading = 'E';
-                        x += distance;
-                    }
-                    break;
-                case 'W':
-                    if (turn == 'R') {
-                        heading = 'N';
-                        y += distance;
-                    } else {
-                        heading = 'S';
-                        y -= distance;
-                    }
-                    break;
-                default:
-                    throw new IllegalStateException(String.format("Impossible heading found %s!", heading));
-            }
-            System.out.println(String.format("Turn: %s, Distance: %d -> (%d, %d)", turn, distance, x, y));
+            position.move(turn, distance);
+            System.out.println(String.format("Turn: %s, Distance: %d -> %s", turn, distance, position));
         }
-        return Math.abs(x) + Math.abs(y);
+        return position.getDistance();
     }
 
     private static void test_blocks_away(final String directions, final int expected_distance) {
@@ -76,7 +98,7 @@ public class Advent1 {
         test_blocks_away("R2, L3", 5);
         test_blocks_away("R2, R2, R2", 2);
         test_blocks_away("R5, L5, R5, R3", 12);
-        String directions = readInputFile("2016/day1/input1.txt");
+        String directions = readInputFile();
         int distance = blocks_away(directions);
         System.out.println(String.format("Day 1, Part 1 the Easter Bunny HQ is %d blocks away.", distance));
     }
