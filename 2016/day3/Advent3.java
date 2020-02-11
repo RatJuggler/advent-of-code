ac3bb77d434947c09e9c5d7e5661a49eb1f01bbb
John Chase <ratteal@gmail.com>
2020-02-11 18:47:24 +0000

    Test and skeleton code for Part 2.

--- 2016/day3/Advent3.java
+++ 2016/day3/Advent3.java
@@ -9,24 +9,32 @@ import java.util.stream.Stream;
 
 final class TriangleDataValidator {
 
-    private int dataValidations = 0;
     private int validTriangles = 0;
 
     TriangleDataValidator() {}
 
-    void validateData(final String triangleData) {
-        this.dataValidations++;
-        Scanner scan = new Scanner(triangleData);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+    private void validateTriangle(final int a, final int b, final int c) {
         if (a + b > c && a + c > b && b + c > a) {
             this.validTriangles++;
         }
     }
 
-    int getDataValidations() {
-        return this.dataValidations;
+    void validateDataByRow(final String filename) throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream.forEach(this::validateData);
+        }
+    }
+
+    void validateDataByColumn(final String filename) {
+
+    }
+
+    void validateData(final String triangleData) {
+        Scanner scan = new Scanner(triangleData);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        validateTriangle(a, b, c);
     }
 
     int getValidTriangles() {
@@ -37,17 +45,40 @@ final class TriangleDataValidator {
 
 final class Advent3 {
 
-    static int countValidTriangles(final String filename) throws IOException {
+    static int countValidTrianglesByRow(final String filename) throws IOException {
         TriangleDataValidator validator = new TriangleDataValidator();
-        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-            stream.forEach(validator::validateData);
-        }
+        validator.validateDataByRow(filename);
         return validator.getValidTriangles();
     }
 
+    static void testValidateTrianglesByRow(final String testFileName) throws IOException {
+        final int expectedValidTriangles = 3;
+        int validTriangles = countValidTrianglesByRow(testFileName);
+        assert validTriangles == expectedValidTriangles:
+                String.format("Expect to find %d valid triangles but was %d!", expectedValidTriangles, validTriangles);
+    }
+
+    static int countValidTrianglesByColumn(final String filename) throws IOException {
+        TriangleDataValidator validator = new TriangleDataValidator();
+        validator.validateDataByColumn(filename);
+        return validator.getValidTriangles();
+    }
+
+    static void testValidateTrianglesByColumn(final String testFileName) throws IOException {
+        final int expectedValidTriangles = 6;
+        int validTriangles = countValidTrianglesByColumn(testFileName);
+        assert validTriangles == expectedValidTriangles:
+                String.format("Expect to find %d valid triangles but was %d!", expectedValidTriangles, validTriangles);
+    }
+
     public static void main(final String[] args) throws IOException {
-        int validTriangles = countValidTriangles("2016/day3/input3.txt");
-        System.out.println(String.format("Day 3, Part 1, there are %d valid triangles.", validTriangles));
+        String testFileName = "2016/day3/test3a.txt";
+        testValidateTrianglesByRow(testFileName);
+        int validTriangles1 = countValidTrianglesByRow("2016/day3/input3.txt");
+        System.out.println(String.format("Day 3, Part 1, there are %d valid triangles.", validTriangles1));
+        testValidateTrianglesByColumn(testFileName);
+        int validTriangles2 = countValidTrianglesByColumn("2016/day3/input3.txt");
+        System.out.println(String.format("Day 3, Part 2, there are %d valid triangles.", validTriangles2));
     }
 
 }
