ab6ff7b5235fb8961ea61f667df40306f67e4887
John Chase <ratteal@gmail.com>
2020-02-11 16:43:10 +0000

    Day 3 initial skeleton code.

---
+++ 2016/day3/Advent3.java
@@ -0,0 +1,45 @@
+package day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+
+final class TriangleDataValidator {
+
+    private int dataValidations = 0;
+    private int validTriangles = 0;
+
+    TriangleDataValidator() {}
+
+    void validateData(final String triangleData) {
+        this.dataValidations++;
+    }
+
+    int getDataValidations() {
+        return this.dataValidations;
+    }
+
+    int getValidTriangles() {
+        return this.validTriangles;
+    }
+}
+
+
+final class Advent3 {
+
+    static int countValidTriangles(final String filename) throws IOException {
+        TriangleDataValidator validator = new TriangleDataValidator();
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream.forEach(validator::validateData);
+        }
+        return validator.getValidTriangles();
+    }
+
+    public static void main(final String[] args) throws IOException {
+        int validTriangles = countValidTriangles("2016/day3/input3.txt");
+        System.out.println(String.format("Day 3, Part 1, there are %d valid triangles.", validTriangles));
+    }
+
+}
