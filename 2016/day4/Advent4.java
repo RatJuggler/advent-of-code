e82c61da92a9eba33fa563935f6b478915935e93
John Chase <ratteal@gmail.com>
2020-02-12 15:00:11 +0000

    Solution to Part 1.

--- 2016/day4/Advent4.java
+++ 2016/day4/Advent4.java
@@ -1,10 +1,14 @@
 package day4;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
@@ -42,7 +46,11 @@ class RoomValidator {
 
     boolean isARealRoom() {
         Map<Character, Long> occurrences = countLetterOccurrences(this.name, this.checksum.length());
-        return true;
+        return this.checksum.codePoints().mapToObj(c -> (char) c).allMatch(occurrences::containsKey);
+    }
+
+    long sectorIdValue() {
+        return Long.parseLong(this.sectorId);
     }
 
     @Override
@@ -50,6 +58,7 @@ class RoomValidator {
         return String.format("RoomValidator{name='%s', sectorId='%s', checksum='%s'}", this.name, this.sectorId, this.checksum);
     }
 }
+
 
 public class Advent4 {
 
@@ -59,16 +68,23 @@ public class Advent4 {
         return roomValidator.isARealRoom();
     }
 
+    private static Long sumRealRoomSectorIds() throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get("2016/day4/input4.txt"))) {
+            return stream.map(RoomValidator::new).filter(RoomValidator::isARealRoom).map(RoomValidator::sectorIdValue).reduce(0L, Long::sum);
+        }
+    }
+
     private static void testIsARealRoom(final String room, final boolean expectedRealRoom) {
         boolean realRoom = isARealRoom(room);
         assert realRoom == expectedRealRoom: String.format("Expected result %s but was %s!", expectedRealRoom, realRoom);
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
         testIsARealRoom("aaaaa-bbb-z-y-x-123[abxyz]", true);
         testIsARealRoom("a-b-c-d-e-f-g-h-987[abcde]", true);
         testIsARealRoom("not-a-real-room-404[oarel]", true);
         testIsARealRoom("totally-real-room-200[decoy]", false);
+        System.out.println(String.format("Day 4, Step 1, sum of sector id's for real rooms is %d.", sumRealRoomSectorIds()));
     }
 
 }
