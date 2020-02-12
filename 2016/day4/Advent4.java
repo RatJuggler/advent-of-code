a776dc607feaf93312194b4344d9f057ba289002
John Chase <ratteal@gmail.com>
2020-02-12 12:20:46 +0000

    Parse the room details.

--- 2016/day4/Advent4.java
+++ 2016/day4/Advent4.java
@@ -1,9 +1,22 @@
 package day4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Advent4 {
 
     private static boolean isARealRoom(String room) {
+        String pattern = "(?<name>[a-z\\-]+)-(?<sectorid>\\d+)\\[(?<checksum>[a-z]+)]";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(room);
+        if (!m.find()) {
+            throw new IllegalStateException("Unable to parse room: " + room);
+        }
+        String name = m.group("name");
+        String sectorId = m.group("sectorid");
+        String checksum = m.group("checksum");
+        System.out.println(String.format("%s -> %s %s %s", room, name, sectorId, checksum));
         return false;
     }
 
