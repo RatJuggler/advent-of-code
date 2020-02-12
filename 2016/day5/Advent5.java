260ea4a069431d2b6d5ff238590d021041050f38
John Chase <ratteal@gmail.com>
2020-02-12 21:38:30 +0000

    Test for Part 2.

--- 2016/day5/Advent5.java
+++ 2016/day5/Advent5.java
@@ -7,8 +7,8 @@ import java.security.NoSuchAlgorithmException;
 public class Advent5 {
 
     private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
-    
-    private static String bytesToHex(byte[] bytes) {
+
+    private static String bytesToHex(final byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
@@ -17,8 +17,13 @@ public class Advent5 {
         }
         return new String(hexChars);
     }
-    
-    private static String hackPassword(String doorId) throws NoSuchAlgorithmException {
+
+    private enum PasswordCoding {
+        SEQUENTIAL,
+        POSITIONAL
+    }
+
+    private static String hackPassword(final String doorId, final PasswordCoding coding) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
         StringBuilder password = new StringBuilder();
         int index = 0;
@@ -32,22 +37,26 @@ public class Advent5 {
                 hash = bytesToHex(digest);
             } while (!hash.startsWith("00000"));
             System.out.println(hash);
-            password.append(hash.charAt(5));
+            if (coding == PasswordCoding.SEQUENTIAL) {
+                password.append(hash.charAt(5));
+            } else {
+
+            }
         }
         return password.toString();
     }
 
-    private static void testHackPassword() throws NoSuchAlgorithmException {
+    private static void testHackPassword(final String expectedPassword, final PasswordCoding coding) throws NoSuchAlgorithmException {
         String doorId = "abc";
-        String expectedPassword = "18f47a30";
-        String password = hackPassword(doorId);
+        String password = hackPassword(doorId, coding);
         assert password.equals(expectedPassword) : String.format("Expected password '%s' but was '%s'!", expectedPassword, password);
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        testHackPassword();
-        String password = hackPassword("abbhdwsy");
+        testHackPassword("18f47a30", PasswordCoding.SEQUENTIAL);
+        String password = hackPassword("abbhdwsy", PasswordCoding.SEQUENTIAL);
         System.out.println(String.format("Day 5, Part 1 the password is '%s'.", password));
+        testHackPassword("05ace8e3", PasswordCoding.POSITIONAL);
     }
 
 }
