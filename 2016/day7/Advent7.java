399532d6a3780941d63d140e79c765581a1897a3
John Chase <ratteal@gmail.com>
2020-02-13 17:48:33 +0000

    Start building an IP validator.

--- 2016/day7/Advent7.java
+++ 2016/day7/Advent7.java
@@ -1,10 +1,44 @@
 package day7;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+class IPValidator {
+
+    private final String prenet;
+    private final String hypernet;
+    private final String postnet;
+
+    IPValidator(final String ip) {
+        String pattern = "(?<prenet>[a-z]+)\\[(?<hypernet>[a-z]+)](?<postnet>[a-z]+)";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(ip);
+        if (!m.find()) {
+            throw new IllegalStateException("Unable to parse IP: " + ip);
+        }
+        this.prenet = m.group("prenet");
+        this.hypernet = m.group("hypernet");
+        this.postnet = m.group("postnet");
+    }
+
+    boolean supportsTLS() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("IPValidator{prenet='%s', hypernet='%s', postnet='%s'}", this.prenet, this.hypernet, this.postnet);
+    }
+}
+
 
 public class Advent7 {
 
     private static boolean supportsTLS(final String ip) {
-        return true;
+        IPValidator validator = new IPValidator(ip);
+        System.out.println(validator);
+        return validator.supportsTLS();
     }
 
     private static void testSupportsTLS(final String ip, final boolean expectedSupports) {
