e97a7d3c18b51451fbf3aa2ad3b7bb11da6d8315
John Chase <ratteal@gmail.com>
2020-02-13 15:17:08 +0000

    Start trying to use streams instead.

--- 2016/day6/Advent6.java
+++ 2016/day6/Advent6.java
@@ -2,35 +2,39 @@ package day6;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 public class Advent6 {
 
-    private static String decodeMessage(final String filename) throws IOException {
-        int[][] occurrences = new int[6][26];
-        for (int i = 0; i < 6; i++) {
+    private static String decodeMessage(final String filename, final int messageWidth) throws IOException {
+        int[][] occurrences = new int[messageWidth][26];
+        for (int i = 0; i < messageWidth; i++) {
             Arrays.fill(occurrences[i], 0);
         }
-        Scanner scanner = new Scanner(new File(filename));
-        while (scanner.hasNextLine()) {
-            String row = scanner.nextLine();
-            for (int i = 0; i < 6; i++) {
-                occurrences[i][row.charAt(i) - 'a']++;
-            }
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream.map(String::toCharArray)
+                    .forEach(r -> IntStream.range(0, messageWidth)
+                            .mapToObj(i -> new int[] {i, r[i] - 'a'})
+                            .forEach(j -> occurrences[j[0]][j[1]]++));
+        }
+        for (int i = 0; i < messageWidth; i++) {
+            System.out.println(Arrays.toString(occurrences[i]));
         }
         StringBuilder message = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            int highestOccurrences = 0;
-            int atIndex = 0;
-            for (int j = 0; j < 26; j++) {
-                if (occurrences[i][j] > highestOccurrences) {
-                    highestOccurrences = occurrences[i][j];
-                    atIndex = j;
+        for (int i = 0; i < messageWidth; i++) {
+            int highestOccurrenceAt = 0;
+            for (int j = 0; j < occurrences[i].length; j++) {
+                if (occurrences[i][j] > occurrences[i][highestOccurrenceAt]) {
+                    highestOccurrenceAt = j;
                 }
             }
-            message.append((char) ('a' + atIndex));
+            message.append((char) ('a' + highestOccurrenceAt));
         }
         return message.toString();
     }
@@ -38,7 +42,7 @@ public class Advent6 {
     private static void testDecodeMessage() throws IOException {
         String filename = "2016/day6/test6a.txt";
         String expectedMessage = "easter";
-        String message = decodeMessage(filename);
+        String message = decodeMessage(filename, expectedMessage.length());
         assert message.equals(expectedMessage) : String.format("Expect message '%s' but was '%s'!", expectedMessage, message);
     }
 
