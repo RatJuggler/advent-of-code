4b9ee2ae58b1903b7edebb13c2d0c7c21bab21b7
John Chase <ratteal@gmail.com>
2020-02-14 17:18:38 +0000

    Start to build a screen class,

--- 2016/day8/Advent8.java
+++ 2016/day8/Advent8.java
@@ -1,19 +1,70 @@
 package day8;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+class Screen {
+
+    private final static char PIXEL_ON = '#';
+    private final static char PIXEL_OFF = '.';
+
+    private final List<char[]> display = new ArrayList<>();
+
+    public Screen(final int width, final int height) {
+        for (int i = 0; i < height; i++) {
+            char[] row = new char[width];
+            Arrays.fill(row, PIXEL_OFF);
+            this.display.add(row);
+        }
+    }
+
+    void applyInstruction(final String instruction) {
+
+    }
+
+    int countLitPixels() {
+        int litPixels = 0;
+        for (char[] row : this.display) {
+            litPixels += IntStream
+                    .range(0, row.length)
+                    .mapToObj(c -> row[c])
+                    .filter(c -> c == PIXEL_ON)
+                    .count();
+        }
+        return litPixels;
+    }
+
+}
+
+
 public class Advent8 {
 
-    private static int swipeCard(final String filename) {
-        return 0;
+    private static int swipeCard(final String filename, final int width, final int height) throws IOException {
+        Screen screen = new Screen(width, height);
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream.forEach(screen::applyInstruction);
+        }
+        return screen.countLitPixels();
     }
 
-    private static void testSwipeCard() {
+    private static void testSwipeCard() throws IOException {
         String filename = "2016/day8/test8a.txt";
-        int expectedPixels = 6;
-        int pixels = swipeCard(filename);
-        assert pixels == expectedPixels: String.format("Expected %d pixels to be lit but was %d!", expectedPixels, pixels);
+        int expectedLitPixels = 6;
+        int litPixels = swipeCard(filename, 7, 3);
+        assert litPixels == expectedLitPixels:
+                String.format("Expected %d pixels to be lit but was %d!", expectedLitPixels, litPixels);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         testSwipeCard();
+        int litPixels = swipeCard("2016/day8/input8.txt", 50, 6);
+        System.out.println(String.format("Day 8, Part 1 found %d pixels lit.", litPixels));
     }
 }
