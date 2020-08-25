6a812c8b18054211a45961d4fa6884000fa1e6fb
John Chase <ratteal@gmail.com>
2020-08-25 16:54:19 +0100

    Refactor Part 1 to sum instead of preserving the resulting strings.

--- 2016/day9/Advent9.java
+++ 2016/day9/Advent9.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class Advent9 {
 
-    private static String parseDecompress(final String sequence) {
+    private static int parseDecompress(final String sequence) {
         String pattern = "^\\((?<take>\\d+)x(?<repeat>\\d+)\\)(?<remaining>.*)";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(sequence);
@@ -20,42 +20,41 @@ public class Advent9 {
         int repeat = Integer.parseInt(m.group("repeat"));
         String remaining = m.group("remaining");
         String decompressed = remaining.substring(0, take).repeat(repeat);
-        return decompressed + decompress(remaining.substring(take));
+        return decompressed.length() + decompress(remaining.substring(take));
     }
 
-    private static String decompress(final String sequence) {
+    private static int decompress(final String sequence) {
         System.out.println(sequence);
         if (sequence.isEmpty()) {
-            return sequence;
+            return 0;
         }
         char process = sequence.charAt(0);
         if (process == '(') {
             return parseDecompress(sequence);
         }
-        return process + decompress(sequence.substring(1));
+        return 1 + decompress(sequence.substring(1));
     }
 
-    private static void testDecompress(final String sequence, final String expectedResult) {
-        String decompressed = decompress(sequence);
-        assert decompressed.equals(expectedResult):
-                String.format("Expected decompressed result to be '%s' but was '%s'!", expectedResult, decompressed);
+    private static void testDecompress(final String sequence, final int expectedLength) {
+        int decompressedLength = decompress(sequence);
+        assert decompressedLength == expectedLength:
+                String.format("Expected decompressed length to be %d but was %d!", expectedLength, decompressedLength);
     }
 
     private static Integer decompressedLength(final String filename) throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-            return stream.map(Advent9::decompress)
-                    .mapToInt(String::length)
+            return stream.mapToInt(Advent9::decompress)
                     .sum();
         }
     }
 
     public static void main(final String[] args) throws IOException {
-        testDecompress("ADVENT", "ADVENT");
-        testDecompress("A(1x5)BC", "ABBBBBC");
-        testDecompress("(3x3)XYZ", "XYZXYZXYZ");
-        testDecompress("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG");
-        testDecompress("(6x1)(1x3)A", "(1x3)A");
-        testDecompress("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY");
+        testDecompress("ADVENT", 6);
+        testDecompress("A(1x5)BC", 7);
+        testDecompress("(3x3)XYZ", 9);
+        testDecompress("A(2x2)BCD(2x2)EFG", 11);
+        testDecompress("(6x1)(1x3)A", 6);
+        testDecompress("X(8x2)(3x3)ABCY", 18);
         System.out.printf("Day 9, Part 1 uncompressed length is %d.%n", decompressedLength("2016/day9/input9.txt"));
     }
 
