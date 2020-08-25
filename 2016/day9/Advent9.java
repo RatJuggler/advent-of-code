943d14cd6bb21ecb771aad9f2e280e37ed83e261
John Chase <ratteal@gmail.com>
2020-08-25 16:21:15 +0100

    Part 1 tests passing.

--- 2016/day9/Advent9.java
+++ 2016/day9/Advent9.java
@@ -1,14 +1,40 @@
 package day9;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Advent9 {
 
+    private static String parseDecompress(final String sequence) {
+        String pattern = "^\\((?<take>\\d+)x(?<repeat>\\d+)\\)(?<remaining>.*)";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(sequence);
+        if (!m.find()) {
+            throw new IllegalStateException("Unable to parse decompress sequence: " + sequence);
+        }
+        int take = Integer.parseInt(m.group("take"));
+        int repeat = Integer.parseInt(m.group("repeat"));
+        String remaining = m.group("remaining");
+        String decompressed = remaining.substring(0, take).repeat(repeat);
+        System.out.printf("(%dx%d)%s => %s%n", take, repeat, remaining, decompressed);
+        return decompressed + decompress(remaining.substring(take));
+    }
+
     private static String decompress(final String sequence) {
-        return sequence;
+        System.out.println(sequence);
+        if (sequence.isEmpty()) {
+            return sequence;
+        }
+        char process = sequence.charAt(0);
+        if (process == '(') {
+            return parseDecompress(sequence);
+        }
+        return process + decompress(sequence.substring(1));
     }
 
     private static void testDecompress(final String sequence, final String expectedResult) {
         String decompressed = decompress(sequence);
-        assert sequence.equals(expectedResult):
+        assert decompressed.equals(expectedResult):
                 String.format("Expected decompressed result to be '%s' but was '%s'!", expectedResult, decompressed);
     }
 
