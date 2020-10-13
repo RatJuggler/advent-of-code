7f67e124e3fee9b6a55f288b4554af0788a3c499
John Chase <ratteal@gmail.com>
2020-10-13 17:17:28 +0100

    Rethink solution using strings to represent state.

--- 2016/day11/Advent11.java
+++ 2016/day11/Advent11.java
@@ -1,82 +1,120 @@
 package day11;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-class Floor {
-
-    private final int number;
-    private final List<String> contains;
-
-    Floor(final int number, final int capacity) {
-        this.number = number;
-        this.contains = new ArrayList<>(capacity);
-    }
-
-    public void addComponent(String component) {
-        this.contains.add(component);
-    }
-
-    @Override
-    public String toString() {
-        return "Floor-" + number + "{contains=" + contains + '}';
-    }
-
-}
+import java.util.Objects;
 
 class Column {
 
-    private final Floor[] floors;
+    private static final List<Column> history = new ArrayList<>();
 
-    Column(final int floors, final int capacity) {
-        this.floors = new Floor[floors];
-        for (int i = 0; i < floors; i++) {
-            this.floors[i] = new Floor(i, capacity);
+    private final String[] floors;
+    private final int elevator;
+
+    Column(final String[] floors, final int elevator) {
+        this.floors = floors;
+        this.elevator = elevator;
+    }
+
+    static boolean newState(final Column newColumn) {
+        for (Column column : Column.history) {
+            if (newColumn.equals(column)) return false;
+        }
+        Column.history.add(newColumn);
+        return true;
+    }
+
+    static void dump() {
+        for (Column column : Column.history) {
+            System.out.println(column);
+        }
+    }
+    private boolean validMove(final String component, final String floor, final char componentMoved, final String otherComponent) {
+        return component.charAt(1) == componentMoved && (!floor.contains(otherComponent) || floor.contains(component.charAt(0) + otherComponent));
+    }
+
+    private void moveComponent(final int newElevator) {
+        String oldFloor = this.floors[this.elevator];
+        String newFloor = this.floors[newElevator];
+        for (int i = 0; i < (oldFloor.length() - 4) / 3; i++) {
+            int componentAt = 5 + (i * 3);
+            String component = oldFloor.substring(componentAt, componentAt + 2);
+            if (component.equals("..")) continue;
+            // Microchip moved to the same floor as an incompatible Generator OR Generator moved to the same floor as an incompatible Microchip.
+            if (this.validMove(component, newFloor, 'M', "G") ||
+                    this.validMove(component, newFloor, 'G', "M")) {
+                String[] newFloors = this.floors.clone();
+                newFloors[this.elevator] = oldFloor.substring(0, 3) + "." + oldFloor.substring(4, componentAt) + ".." + oldFloor.substring(componentAt + 2);
+                newFloors[newElevator] = newFloor.substring(0, 3) + "E" + newFloor.substring(4, componentAt) + component + newFloor.substring(componentAt + 2);
+                Column newColumn = new Column(newFloors, newElevator);
+                if (Column.newState(newColumn)) newColumn.move();
+            }
         }
     }
 
-    void addComponent(final int floor, final String component) {
-        this.floors[floor].addComponent(component);
+    void move() {
+        if (this.floors[this.floors.length - 1].contains("..")) {
+            if (this.elevator > 0) this.moveComponent(this.elevator - 1);
+            if (this.elevator < this.floors.length - 1) this.moveComponent(this.elevator + 1);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Column column = (Column) o;
+        return elevator == column.elevator &&
+                Arrays.equals(floors, column.floors);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(elevator);
+        result = 31 * result + Arrays.hashCode(floors);
+        return result;
     }
 
     @Override
     public String toString() {
-        return "Column{" +
-                "floors=" + Arrays.toString(floors) +
-                '}';
+        StringBuilder column = new StringBuilder();
+        for (int i = this.floors.length; i > 0; i--) {
+            column.append(this.floors[i - 1]).append('\n');
+        }
+        return column.toString();
     }
 }
 
 public class Advent11 {
 
     private static void testColumn() {
-        Column column = new Column(4, 4);
-        column.addComponent(0, "HM");
-        column.addComponent(0, "LM");
-        column.addComponent(1, "HG");
-        column.addComponent(2, "LG");
-        System.out.println(column);
+        String[] floors = new String[4];
+        floors[3] = "F4 . .. .. .. ..";
+        floors[2] = "F3 . .. .. LG ..";
+        floors[1] = "F2 . HG .. .. ..";
+        floors[0] = "F1 E .. HM .. LM";
+        Column column = new Column(floors, 0);
+        if (Column.newState(column)) column.move();
+        Column.dump();
     }
 
+
     private static void part1Column() {
-        Column column = new Column(4, 4);
-        column.addComponent(0, "POG");
-        column.addComponent(0, "TG");
-        column.addComponent(0, "TM");
-        column.addComponent(0, "PRG");
-        column.addComponent(0, "RG");
-        column.addComponent(0, "RM");
-        column.addComponent(0, "CG");
-        column.addComponent(0, "CM");
-        column.addComponent(1, "POM");
-        column.addComponent(1, "PRM");
-        System.out.println(column);
+        String[] floors = new String[4];
+        floors[3] = "F4 . .. .. .. .. .. .. .. .. .. ..";
+        floors[2] = "F3 . .. .. .. .. .. .. .. .. .. ..";
+        floors[1] = "F2 . .. .. .. OM .. PM .. .. .. ..";
+        floors[0] = "F1 E CG CM OG .. PG .. RG RM TG TM";
+        Column column = new Column(floors, 0);
+        if (Column.newState(column)) column.move();
+        Column.dump();
     }
 
     public static void main(final String[] args) {
         testColumn();
-        part1Column();
+//        part1Column();
     }
 
 }
