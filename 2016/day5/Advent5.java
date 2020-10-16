8ab9d47061ee57fd238f7a14a2541dd4eb15c689
John Chase <ratteal@gmail.com>
2020-10-16 19:26:16 +0100

    Improved hex to string function.

--- 2016/day5/Advent5.java
+++ 2016/day5/Advent5.java
@@ -6,16 +6,12 @@ import java.security.NoSuchAlgorithmException;
 
 public class Advent5 {
 
-    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
-
     private static String bytesToHex(final byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1));
         }
-        return String.valueOf(hexChars);
+        return sb.toString();
     }
 
     private static String hackPassword(final String doorId, final boolean sequential) throws NoSuchAlgorithmException {
