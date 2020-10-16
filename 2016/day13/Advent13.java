6e74ae141b5acc9736da740ee118d69379c2a97d
John Chase <ratteal@gmail.com>
2020-10-16 15:26:28 +0100

    Need to implement path finding.

--- 2016/day13/Advent13.java
+++ 2016/day13/Advent13.java
@@ -22,15 +22,22 @@ class Office {
         }
     }
 
+    int findPathTo(final int toX, final int toY) {
+        return 0;
+    }
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder(this.space.length).append("  ");
+        StringBuilder sb = new StringBuilder(this.space.length * this.space[0].length);
+        StringBuilder row1 = new StringBuilder().append("   ");
+        StringBuilder row2 = new StringBuilder().append("   ");
         for (int x = 0; x < this.space[0].length; x++) {
-            sb.append(x);
+            row1.append(x / 10);
+            row2.append(x % 10);
         }
-        sb.append('\n');
+        sb.append(row1).append('\n').append(row2).append('\n');
         for (int y = 0; y < this.space.length; y++) {
-            sb.append(y).append(' ').append(this.space[y]).append('\n');
+            sb.append(String.format("%02d ", y)).append(this.space[y]).append('\n');
         }
         return sb.toString();
     }
@@ -44,11 +51,17 @@ public class Advent13 {
     }
 
     private static void part1() {
+        Office office = new Office(1358, 50, 50);
+        System.out.println(office.toString());
+        System.out.printf("Part 1, steps taken = %s\n", office.findPathTo(31, 39));
     }
 
     private static void test() {
         Office office = new Office(10, 10, 7);
         System.out.println(office.toString());
+        int expectedSteps = 11;
+        int actualSteps = office.findPathTo(7, 4);
+        assert actualSteps == expectedSteps : String.format("Expected to take '%s' steps but was '%s'!", expectedSteps, actualSteps);
     }
 
     public static void main(final String[] args) {
