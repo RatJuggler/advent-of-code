d6eb43f4b8beb66f7bf36d557f746490132d192d
John Chase <ratteal@gmail.com>
2020-10-16 12:04:26 +0100

    Test passes but not returning correct solution for Part 1.

--- 2016/day12/Advent12.java
+++ 2016/day12/Advent12.java
@@ -3,12 +3,15 @@ package day12;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 class Computer {
 
     private final List<String> program;
+    private final Map<String, Integer> registers = new HashMap<>();
 
     Computer(final List<String> program) {
         this.program = program;
@@ -19,9 +22,42 @@ class Computer {
         return new Computer(program);
     }
 
+    int getRegister(final String register) {
+        return this.registers.getOrDefault(register, 0);
+    }
+
     void run() {
-        for (String instruction : this.program) {
-            System.out.println(instruction);
+        int line = 0;
+        while (line < this.program.size() - 1) {
+            String instruction = this.program.get(line);
+            String[] decode = instruction.split(" ");
+            switch (decode[0]) {
+                case "cpy":
+                    int copy;
+                    if (Character.isAlphabetic(decode[1].charAt(0)))
+                        copy = this.registers.getOrDefault(decode[1], 0);
+                    else
+                        copy = Integer.parseInt(decode[1]);
+                    this.registers.put(decode[2], copy);
+                    line++;
+                    break;
+                case "inc":
+                    this.registers.put(decode[1], this.registers.getOrDefault(decode[1], 0) + 1);
+                    line++;
+                    break;
+                case "dec":
+                    this.registers.put(decode[1], this.registers.getOrDefault(decode[1], 0) - 1);
+                    line++;
+                    break;
+                case "jnz":
+                    if (this.registers.getOrDefault(decode[1], 0) != 0)
+                        line += Integer.parseInt(decode[2]);
+                    else
+                        line++;
+                    break;
+                default:
+                    throw new IllegalStateException(String.format("Unknown command '%s'", decode[0]));
+            }
         }
     }
 
@@ -36,11 +72,15 @@ public class Advent12 {
     private static void part1() throws IOException {
         Computer computer = Computer.fromFile("2016/day12/input12.txt");
         computer.run();
+        System.out.printf("Register 'a' = %s", computer.getRegister("a"));
     }
 
     private static void test() throws IOException {
+        int expectedA = 42;
         Computer computer = Computer.fromFile("2016/day12/test12.txt");
         computer.run();
+        int actualA = computer.getRegister("a");
+        assert actualA == expectedA : String.format("Expected register 'a' to be '%s' but was '%s'!", expectedA, actualA);
     }
 
     public static void main(final String[] args) throws IOException {
