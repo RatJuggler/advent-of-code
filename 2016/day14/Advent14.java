6ffca474316df6619acdd30ae9ae2ab27ec99fe2
John Chase <ratteal@gmail.com>
2020-10-17 10:43:33 +0100

    Start to cache the hashes for searching.

--- 2016/day14/Advent14.java
+++ 2016/day14/Advent14.java
@@ -3,26 +3,20 @@ package day14;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-class KeyGenerator {
+class HashCache {
 
     private final String salt;
+    private final Deque<String> cache;
+    private int cacheIndex;
+    private int nextIndex;
 
-    KeyGenerator(final String salt) {
-        this.salt = salt;
-    }
-
-    private boolean tripleFound(final String hash) {
-        String pattern = "(?<triple>.)\\1{2}";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(hash);
-        return m.find();
-    }
-
-    private String md5(String input) throws NoSuchAlgorithmException {
+    private static String md5(String input) throws NoSuchAlgorithmException {
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         byte[] digest = md5.digest(input.getBytes(StandardCharsets.UTF_8));
         StringBuilder sb = new StringBuilder();
@@ -32,15 +26,49 @@ class KeyGenerator {
         return sb.toString();
     }
 
+    HashCache(final String salt, final int size) throws NoSuchAlgorithmException {
+        this.salt = salt;
+        this.cache = new ArrayDeque<>(size);
+        for (int i = 0; i < size; i++) {
+            this.cache.add(HashCache.md5(salt + i));
+        }
+        this.cacheIndex = this.cache.size();
+        this.nextIndex = -1;
+    }
+
+    String nextHash() throws NoSuchAlgorithmException {
+        this.cache.add(HashCache.md5(salt + this.cacheIndex));
+        this.cacheIndex++;
+        this.nextIndex++;
+        return this.cache.pollFirst();
+    }
+
+    int index() {
+        return this.nextIndex;
+    }
+}
+
+class KeyGenerator {
+
+    private final HashCache hashCache;
+
+    KeyGenerator(final HashCache hashCache) {
+        this.hashCache = hashCache;
+    }
+
+    private boolean tripleFound(final String hash) {
+        String pattern = "(?<triple>.)\\1{2}";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(hash);
+        return m.find();
+    }
+
     int findTriple() throws NoSuchAlgorithmException {
-        int index = -1;
-        String hash = "";
+        String hash;
         do {
-            index++;
-            hash = md5(this.salt + index);
-            System.out.println(index + " " + hash);
+            hash = this.hashCache.nextHash();
         } while (!this.tripleFound(hash));
-        return index;
+        return this.hashCache.index();
     }
 }
 
@@ -55,7 +83,8 @@ public class Advent14 {
 
     private static void test() throws NoSuchAlgorithmException {
         int expectedIndex = 18;
-        KeyGenerator generator = new KeyGenerator("abc");
+        HashCache hashCache = new HashCache("abc", 1000);
+        KeyGenerator generator = new KeyGenerator(hashCache);
         int indexFound = generator.findTriple();
         assert indexFound == expectedIndex : String.format("Expected index to be '%s' but was '%s'!", expectedIndex, indexFound);
     }
