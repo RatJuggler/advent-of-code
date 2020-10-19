a3a9abaaa522ec35dfb5f7d0e295a1d5a25f11db
John Chase <ratteal@gmail.com>
2020-10-19 19:09:01 +0100

    Part 2 test failing.

--- 2016/day17/Advent17.java
+++ 2016/day17/Advent17.java
@@ -7,6 +7,7 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.function.BiFunction;
 
 
 class Position {
@@ -92,18 +93,18 @@ class Vault {
         return nextPositions;
     }
 
-    String navigate() {
+    final String navigate(final int initialPathLength, final BiFunction<String, Integer, Boolean> pathLengthCheck) {
         Queue<Position> nextPositions = new ArrayDeque<>();
-        int shortestPath = Integer.MAX_VALUE;
+        int pathLength = initialPathLength;
         String path = null;
         nextPositions.add(new Position(0, 0, ""));
         while (nextPositions.peek() != null) {
             Position position = nextPositions.poll();
-            if (position.path.length() < shortestPath) {
+            if (pathLengthCheck.apply(position.path, pathLength)) {
                 if (position.x == this.sizeX && position.y == this.sizeY) {
                     path = position.path;
-                    shortestPath = path.length();
-                    System.out.println("Queue: " + nextPositions.size() + ", Shortest Path: " + shortestPath + ", " + path);
+                    pathLength = path.length();
+                    System.out.println("Queue: " + nextPositions.size() + ", Path: " + pathLength + ", " + path);
                 } else {
                     nextPositions.addAll(this.generateMoves(position));
                 }
@@ -111,25 +112,41 @@ class Vault {
         }
         return path;
     }
+
+    String navigateLongestPath() {
+        return this.navigate(0, (path, longestPath) -> path.length() > longestPath);
+    }
+
+    String navigateShortestPath() {
+        return this.navigate(Integer.MAX_VALUE, (path, shortestPath) -> path.length() < shortestPath);
+    }
 }
 
 
 public class Advent17 {
 
     private static void part1() {
-        String path = new Vault("dmypynyp", 3, 3).navigate();
+        String path = new Vault("dmypynyp", 3, 3).navigateShortestPath();
         System.out.printf("Part 1, shortest path found = %s\n", path);
     }
 
-    private static void test(final String passcode, final String shortestPath) {
-        String path = new Vault(passcode, 3, 3).navigate();
+    private static void test2(final String passcode, final int longestPath) {
+        String path = new Vault(passcode, 3, 3).navigateLongestPath();
+        assert path.length() == longestPath : String.format("Expected longest path to be '%s' but was '%s'!", longestPath, path.length());
+    }
+
+    private static void test1(final String passcode, final String shortestPath) {
+        String path = new Vault(passcode, 3, 3).navigateShortestPath();
         assert path.equals(shortestPath) : String.format("Expected shortest path to be '%s' but was '%s'!", shortestPath, path);
     }
 
     public static void main(String[] args) {
-        test("ihgpwlah", "DDRRRD");
-        test("kglvqrro", "DDUDRLRRUDRD");
-        test("ulqzkmiv", "DRURDRUDDLLDLUURRDULRLDUUDDDRR");
+        test1("ihgpwlah", "DDRRRD");
+        test1("kglvqrro", "DDUDRLRRUDRD");
+        test1("ulqzkmiv", "DRURDRUDDLLDLUURRDULRLDUUDDDRR");
         part1();
+        test2("ihgpwlah", 370);
+        test2("kglvqrro", 492);
+        test2("ulqzkmiv", 830);
     }
 }
