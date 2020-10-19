ca0d848bb50045f260d87132246e80c4fcb71909
John Chase <ratteal@gmail.com>
2020-10-19 10:40:36 +0100

    Load discs from file to form a sculpture.

--- 2016/day15/Advent15.java
+++ 2016/day15/Advent15.java
@@ -1,5 +1,13 @@
 package day15;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 
 class Disc {
 
@@ -11,9 +19,47 @@ class Disc {
         this.currentPosition = startPosition;
     }
 
+    static Disc fromLine(final String line) {
+        String pattern = "Disc #(?<disc>\\d+) has (?<positions>\\d+) positions; at time=0, it is at position (?<startPosition>\\d+).";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (!m.find()) {
+            throw new IllegalStateException("Unable to parse disc line: " + line);
+        }
+        int positions = Integer.parseInt(m.group("positions"));
+        int startPosition = Integer.parseInt(m.group("startPosition"));
+        return new Disc(positions, startPosition);
+    }
+
     int turn() {
         this.currentPosition = (this.currentPosition + 1) % this.positions;
         return this.currentPosition;
+    }
+
+    @Override
+    public String toString() {
+        return "Disc{positions=" + positions + ", currentPosition=" + currentPosition + '}';
+    }
+}
+
+
+class Sculpture {
+
+    private final Disc[] discs;
+
+    Sculpture(final Disc[] discs) { this.discs = discs; }
+
+    static Sculpture fromFile(final String filename) throws IOException {
+        Disc[] discs;
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            discs = stream.map(Disc::fromLine).toArray(Disc[]::new);
+        }
+        return new Sculpture(discs);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(discs);
     }
 }
 
@@ -23,15 +69,12 @@ public class Advent15 {
     private static void part1() {
     }
 
-    private static void test() {
-        Disc disc = new Disc(3, 0);
-        System.out.println(disc.turn());
-        System.out.println(disc.turn());
-        System.out.println(disc.turn());
-        System.out.println(disc.turn());
+    private static void test() throws IOException {
+        Sculpture sculpture = Sculpture.fromFile("2016/day15/test15a.txt");
+        System.out.println(sculpture.toString());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         test();
         part1();
     }
