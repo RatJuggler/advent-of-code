a83bd482c61dd9dbd14196b6e9b702e32d31f55b
John Chase <ratteal@gmail.com>
2020-12-02 09:15:01 +0000

    Parsing the password line.

--- 2020/day2/Advent2020Day2.java
+++ 2020/day2/Advent2020Day2.java
@@ -1,11 +1,49 @@
 package day2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 class PasswordValidator {
 
+    class PasswordLine {
+
+        final int min;
+        final int max;
+        final String c;
+        final String password;
+
+        PasswordLine(final int min, final int max, final String c, final String password) {
+            this.min = min;
+            this.max = max;
+            this.c = c;
+            this.password = password;
+        }
+
+        public String toString() {
+            return String.format("{min = %d, max = %d, c = %s, password = %s}", this.min, this.max, this.c, this.password);
+        }
+    }
+
     PasswordValidator() {}
 
+    private PasswordLine parsePasswordLine(final String line) {
+        String pattern = "^(?<min>\\d+)-(?<max>\\d+) (?<c>\\w): (?<password>\\w+)$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (!m.find()) {
+            throw new IllegalStateException("Unable to parse password line: " + line);
+        }
+        int min = Integer.parseInt(m.group("min"));
+        int max = Integer.parseInt(m.group("max"));
+        String c = m.group("c");
+        String password = m.group("password");
+        return new PasswordLine(min, max, c, password);
+    }
+
     boolean validate(final String passwordLine) {
+        PasswordLine line = this.parsePasswordLine(passwordLine);
+        System.out.println(line);
         return false;
     }
 }
