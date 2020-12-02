efe02a03863cc33bb2f53a358747618afb4b38c6
John Chase <ratteal@gmail.com>
2020-12-02 10:08:52 +0000

    Day 1, part 1 test and solution.

--- 2020/day2/Advent2020Day2.java
+++ 2020/day2/Advent2020Day2.java
@@ -1,37 +1,28 @@
 package day2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 
 class PasswordValidator {
 
-    static class PasswordLine {
+    final int min;
+    final int max;
+    final String c;
+    final String password;
 
-        final int min;
-        final int max;
-        final String c;
-        final String password;
-
-        PasswordLine(final int min, final int max, final String c, final String password) {
-            this.min = min;
-            this.max = max;
-            this.c = c;
-            this.password = password;
-        }
-
-        String getRegExRule() {
-            return String.format("(%s){%d,%d}", this.c, this.min, this.max);
-        }
-
-        public String toString() {
-            return String.format("{min = %d, max = %d, c = %s, password = %s}", this.min, this.max, this.c, this.password);
-        }
+    PasswordValidator(final int min, final int max, final String c, final String password) {
+        this.min = min;
+        this.max = max;
+        this.c = c;
+        this.password = password;
     }
 
-    PasswordValidator() {}
-
-    private PasswordLine parsePasswordLine(final String line) {
+    static PasswordValidator fromPasswordLine(final String line) {
         String pattern = "^(?<min>\\d+)-(?<max>\\d+) (?<c>\\w): (?<password>\\w+)$";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(line);
@@ -42,18 +33,18 @@ class PasswordValidator {
         int max = Integer.parseInt(m.group("max"));
         String c = m.group("c");
         String password = m.group("password");
-        return new PasswordLine(min, max, c, password);
+        return new PasswordValidator(min, max, c, password);
     }
 
-    private boolean checkRule(final String pattern, final String password) {
+    boolean validate() {
+        String pattern = String.format("(%s)", this.c);
         Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(password);
-        return m.find();
+        long found = r.matcher(this.password).results().count();
+        return found >= this.min && found <= this.max;
     }
 
-    boolean validate(final String passwordLine) {
-        PasswordLine details = this.parsePasswordLine(passwordLine);
-        return this.checkRule(details.getRegExRule(), details.password);
+    public String toString() {
+        return String.format("{min = %d, max = %d, c = %s, password = %s}", this.min, this.max, this.c, this.password);
     }
 }
 
@@ -61,17 +52,27 @@ class PasswordValidator {
 public class Advent2020Day2 {
 
     private static void testPasswordValidator() {
-        PasswordValidator validator = new PasswordValidator();
         String testLine1 = "1-3 a: abcde";
-        assert validator.validate(testLine1) : String.format("Expected password in line \"%s\" to be valid!", testLine1);
+        assert PasswordValidator.fromPasswordLine(testLine1).validate() :
+                String.format("Expected password in line \"%s\" to be valid!", testLine1);
         String testLine2 = "1-3 b: cdefg";
-        assert !validator.validate(testLine2) : String.format("Expected password in line \"%s\" to be invalid!", testLine2);
+        assert !PasswordValidator.fromPasswordLine(testLine2).validate() :
+                String.format("Expected password in line \"%s\" to be invalid!", testLine2);
         String testLine3 = "2-9 c: ccccccccc";
-        assert validator.validate(testLine3) : String.format("Expected password in line \"%s\" to be valid!", testLine3);
+        assert PasswordValidator.fromPasswordLine(testLine3).validate() :
+                String.format("Expected password in line \"%s\" to be valid!", testLine3);
     }
 
-    public static void main(final String[] args) {
+    private static long countValidPasswords(final String filename) throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            return stream.map(PasswordValidator::fromPasswordLine).filter(PasswordValidator::validate).count();
+        }
+    }
+
+    public static void main(final String[] args) throws IOException {
         testPasswordValidator();
+        assert countValidPasswords("2020/day2/test2a.txt") == 2 : "Expected valid password count to be 2!";
+        System.out.printf("Day 1, part 1, number of valid passwords is %d.%n", countValidPasswords("2020/day2/input2.txt"));
     }
 
 }
--- 2020/day2/README.md
+++ 2020/day2/README.md
@@ -25,3 +25,5 @@ In the above example, 2 passwords are valid. The middle password, cdefg, is not;
 . The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+Your puzzle answer was 556.
