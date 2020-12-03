41d2724b6172b85d53953b74ef85ccb9ed752122
John Chase <ratteal@gmail.com>
2020-12-03 09:44:40 +0000

    Part 1 test passing.

--- 2020/day3/Advent2020Day3.java
+++ 2020/day3/Advent2020Day3.java
@@ -1,23 +1,44 @@
 package day3;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 class MapOfTrees {
 
-    MapOfTrees() {}
+    private final List<String> map;
+    private final int width;
 
-    static MapOfTrees fromFile(final String filename) {
-        return new MapOfTrees();
+    MapOfTrees(final List<String> map) {
+        this.map = map;
+        this.width = map.get(0).length();
+    }
+
+    static MapOfTrees fromFile(final String filename) throws IOException {
+        List<String> map = Files.readAllLines(Paths.get(filename));
+        return new MapOfTrees(map);
     }
 
     int countTrees(final int slopeX, final int slopeY) {
-        return 0;
+        int treeCount = 0;
+        int mapX = 0;
+        int mapY = 0;
+        while (mapY < this.map.size()) {
+            char mapLocation = this.map.get(mapY).charAt(mapX);
+            if (mapLocation == '#') treeCount++;
+            mapX = (mapX + slopeX) % this.width;
+            mapY += slopeY;
+        }
+        return treeCount;
     }
 }
 
 
 public class Advent2020Day3 {
 
-    static void testPart1TreeCount() {
+    static void testPart1TreeCount() throws IOException {
         int expectedTreeCount = 7;
         MapOfTrees mapOfTrees = MapOfTrees.fromFile("2020/day3/test3a.txt");
         int actualTreeCount = mapOfTrees.countTrees(3, 1);
@@ -25,7 +46,7 @@ public class Advent2020Day3 {
                 String.format("Expected to encounter %d trees not %d!%n", expectedTreeCount, actualTreeCount);
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
         testPart1TreeCount();
     }
 }
