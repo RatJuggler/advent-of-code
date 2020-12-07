76caea280e6f5d8dc3468c48111a3f3f583da839
John Chase <ratteal@gmail.com>
2020-12-07 09:09:42 +0000

    Day 7, part 1 with test file and failing test.

---
+++ 2020/day7/Advent2020Day7.java
@@ -0,0 +1,15 @@
+package day7;
+
+
+public class Advent2020Day7 {
+
+    private static int countContainingBagColours(final String filename) {
+        int colourCount = 0;
+        return colourCount;
+    }
+
+    public static void main(final String[] args) {
+        assert countContainingBagColours("2020/day7/test7a.txt") == 4 : "Expected containing bag colour count to be 4!";
+        System.out.printf("Day 7, part 1, containing bag colour count is %d.%n", countContainingBagColours("2020/day7/input7.txt"));
+    }
+}
