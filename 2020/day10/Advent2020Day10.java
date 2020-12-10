bd8c51def2699573b69c5a9e2ba3c7b609e4bc2d
John Chase <ratteal@gmail.com>
2020-12-10 09:41:16 +0000

    Day 10, part 1 tests passing and solution.

--- 2020/day10/Advent2020Day10.java
+++ 2020/day10/Advent2020Day10.java
@@ -3,7 +3,9 @@ package day10;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -18,7 +20,15 @@ public class Advent2020Day10 {
     }
 
     private static int findDifferencesProduct(final List<Integer> adapters) {
-        return 0;
+        adapters.sort(Integer::compareTo);
+        Map<Integer, Integer> differences = new HashMap<>();
+        differences.put(adapters.get(0), 1);
+        for (int i = 0; i < adapters.size() - 1; i++) {
+            int difference = adapters.get(i + 1) - adapters.get(i);
+            differences.put(difference, differences.computeIfAbsent(difference, d -> 0) + 1);
+        }
+        System.out.println(differences);
+        return differences.get(1) * (differences.get(3) + 1);
     }
 
     private static void testDifferencesProduct(final String filename, final int expectedDifferencesProduct)
@@ -32,5 +42,7 @@ public class Advent2020Day10 {
     public static void main(final String[] args) throws FileNotFoundException {
         testDifferencesProduct("2020/day10/test10a.txt", 35);
         testDifferencesProduct("2020/day10/test10b.txt", 220);
+        List<Integer> adapters = readAdapters("2020/day10/input10.txt");
+        System.out.printf("Day 10, Part 1 difference product is %d.%n", findDifferencesProduct(adapters));
     }
 }
