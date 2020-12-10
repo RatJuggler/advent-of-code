43716ff84c6167592fc857bc1aa4b4e9f9c5a5c2
John Chase <ratteal@gmail.com>
2020-12-10 13:42:18 +0000

    Refactor improvements for clarity.

--- 2020/day10/Advent2020Day10.java
+++ 2020/day10/Advent2020Day10.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 public class Advent2020Day10 {
@@ -17,65 +18,67 @@ public class Advent2020Day10 {
         Scanner scanner = new Scanner(new File(filename));
         while (scanner.hasNextInt())
             adapters.add(scanner.nextInt());
-        // Sort adapters with the outlet at the beginning and the device at the end.
-        adapters.add(0);
-        adapters.sort(Integer::compareTo);
-        adapters.add(adapters.get(adapters.size() - 1) + 3);
         return adapters;
     }
 
-    private static int findDifferencesProduct(final List<Integer> adapters) {
-        Map<Integer, Long> differences = IntStream.range(0, adapters.size() - 1)
-                .mapToObj(i -> adapters.get(i + 1) - adapters.get(i))
-                .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
-        return (int) (differences.get(1) * differences.get(3));
+    private static Stream<Integer> getDifferences(final List<Integer> adapters) {
+        // First sort the adapters with the outlet at the beginning and the device at the end.
+        adapters.add(0);
+        adapters.sort(Integer::compareTo);
+        adapters.add(adapters.get(adapters.size() - 1) + 3);
+        // Then generate a stream of differences.
+        return IntStream.range(0, adapters.size() - 1).mapToObj(i -> adapters.get(i + 1) - adapters.get(i));
+    }
+
+    private static long findDifferencesProduct(final List<Integer> adapters) {
+        Map<Integer, Long> differences = getDifferences(adapters).collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+        return differences.get(1) * differences.get(3);
     }
 
     private static long findArrangements(final List<Integer> adapters) {
-        List<Integer> differences = IntStream.range(0, adapters.size() - 1)
-                .mapToObj(i -> adapters.get(i + 1) - adapters.get(i))
-                .collect(Collectors.toList());
-        System.out.println(differences);
+        List<Integer> differences = getDifferences(adapters).collect(Collectors.toList());
+
+        // 10 11 12 13 14 => 11,12,13 | 11,12 | 11,13 | 12,13 | 11 | 12 | 13 = 7
+        //   1  1  1  1
+        // 10 11 12 13 16 => 11,12,13 | 11,13 | 12,13 | 13 = 4
+        //   1  1  1  3
+        // 10 11 12 15 => 11,12 | 12 = 2
+        //   1  1  3
+
+        // 10 11 14 => No arrangements
+        //   1  3
+        // 10 13 14 => No arrangements
+        //   3  1
+        // 10 13 16 => No arrangements
+        //   3  3
+
+        // (0)  1  4  5  6  7 10 11 12 15 16 19 (22)
+        //    1  3  1  1  1  3  1  1  3  1  3  3
+
+        //    1  3 (1  1  1  3)(1  1  3) 1  3  3 => 4 * 2 = 8
+
         long arrangements = 1;
         int i = 0;
         while (i < differences.size()) {
-            if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 1 && differences.get(i + 3) == 1) {
-                arrangements *= 7;
-                i += 3;
-            } else if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 1 && differences.get(i + 3) == 3) {
-                arrangements *= 4;
-                i += 3;
-            } else if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 3) {
-                arrangements *= 2;
-                i += 2;
-            } else {
+            if (differences.get(i) == 1 && differences.get(i + 1) == 1) {
+                if (differences.get(i + 2) == 1) {
+                    if (differences.get(i + 3) == 1)
+                        arrangements *= 7;
+                    else if (differences.get(i + 3) == 3)
+                        arrangements *= 4;
+                    i++;
+                } else if (differences.get(i + 2) == 3) {
+                    arrangements *= 2;
+                }
                 i++;
             }
-
-            // (0)  1  4  5  6  7 10 11 12 15 16 19 (22)
-            //    1  3  1  1  1  3  1  1  3  1  3  3
-
-            //    1  3 (1  1  1  3)(1  1  3) 1  3  3 => 4 * 2 = 8
-
-            // 10 11 12 13 14 => 11,12,13 | 11,12 | 11,13 | 12,13 | 11 | 12 | 13 = 7
-            //   1  1  1  1
-            // 10 11 12 13 16 => 11,12,13 | 11,13 | 12,13 | 13 = 4
-            //   1  1  1  3
-            // 10 11 12 15 => 11,12 | 12 = 2
-            //   1  1  3
-
-            // 10 11 14
-            //   1  3
-            // 10 13 14
-            //   3  1
-            // 10 13 16
-            //   3  3
+            i++;
         }
         return arrangements;
     }
 
-    private static void testDifferencesProduct(final List<Integer> adapters, final int expectedDifferencesProduct) {
-        int differencesProduct = findDifferencesProduct(adapters);
+    private static void testDifferencesProduct(final List<Integer> adapters, final long expectedDifferencesProduct) {
+        long differencesProduct = findDifferencesProduct(adapters);
         assert differencesProduct == expectedDifferencesProduct :
                 String.format("Expected to find difference product of '%s' but was '%s'!", expectedDifferencesProduct, differencesProduct);
     }
@@ -86,7 +89,7 @@ public class Advent2020Day10 {
                 String.format("Expected to find '%s' arrangements but found '%s'!", expectedArrangements, arrangements);
     }
 
-    private static void testAdapters(final String filename, final int expectedDifferencesProduct, final long expectedArrangements)
+    private static void testAdapters(final String filename, final long expectedDifferencesProduct, final long expectedArrangements)
             throws FileNotFoundException {
         List<Integer> testAdapters = readAdapters(filename);
         testDifferencesProduct(testAdapters, expectedDifferencesProduct);
