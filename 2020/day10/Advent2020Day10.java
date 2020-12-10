6c6a929504ebad3d04ce56267371fdc91155ba59
John Chase <ratteal@gmail.com>
2020-12-10 12:50:01 +0000

    Part 2 tests passing.

--- 2020/day10/Advent2020Day10.java
+++ 2020/day10/Advent2020Day10.java
@@ -32,8 +32,52 @@ public class Advent2020Day10 {
         return (int) (differences.get(1) * differences.get(3));
     }
 
-    private static int findArrangements(final List<Integer> adapters) {
-        return 0;
+    private static long findArrangements(final List<Integer> adapters) {
+        // Sort adapters with the outlet at the beginning and the device at the end.
+        adapters.add(0);
+        adapters.sort(Integer::compareTo);
+        adapters.add(adapters.get(adapters.size() - 1) + 3);
+        // Count the differences.
+        List<Integer> differences = IntStream.range(0, adapters.size() - 1)
+                .mapToObj(i -> adapters.get(i + 1) - adapters.get(i))
+                .collect(Collectors.toList());
+        System.out.println(differences);
+        long arrangements = 1;
+        int i = 0;
+        while (i < differences.size()) {
+            if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 1 && differences.get(i + 3) == 1) {
+                arrangements *= 7;
+                i += 3;
+            } else if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 1 && differences.get(i + 3) == 3) {
+                arrangements *= 4;
+                i += 3;
+            } else if (differences.get(i) == 1 && differences.get(i + 1) == 1 && differences.get(i + 2) == 3) {
+                arrangements *= 2;
+                i += 2;
+            } else {
+                i++;
+            }
+
+            // (0)  1  4  5  6  7 10 11 12 15 16 19 (22)
+            //    1  3  1  1  1  3  1  1  3  1  3  3
+
+            //    1  3 (1  1  1  3)(1  1  3) 1  3  3 => 4 * 2 = 8
+
+            // 10 11 12 13 14 => 11,12,13 | 11,12 | 11,13 | 12,13 | 11 | 12 | 13 = 7
+            //   1  1  1  1
+            // 10 11 12 13 16 => 11,12,13 | 11,13 | 12,13 | 13 = 4
+            //   1  1  1  3
+            // 10 11 12 15 => 11,12 | 12 = 2
+            //   1  1  3
+
+            // 10 11 14
+            //   1  3
+            // 10 13 14
+            //   3  1
+            // 10 13 16
+            //   3  3
+        }
+        return arrangements;
     }
 
     private static void testDifferencesProduct(final List<Integer> adapters, final int expectedDifferencesProduct) {
@@ -42,13 +86,13 @@ public class Advent2020Day10 {
                 String.format("Expected to find difference product of '%s' but was '%s'!", expectedDifferencesProduct, differencesProduct);
     }
 
-    private static void testAdapterArrangements(final List<Integer> adapters, final int expectedArrangements) {
-        int arrangements = findArrangements(adapters);
+    private static void testAdapterArrangements(final List<Integer> adapters, final long expectedArrangements) {
+        long arrangements = findArrangements(adapters);
         assert arrangements == expectedArrangements :
                 String.format("Expected to find '%s' arrangements but found '%s'!", expectedArrangements, arrangements);
     }
 
-    private static void testAdapters(final String filename, final int expectedDifferencesProduct, final int expectedArrangements)
+    private static void testAdapters(final String filename, final int expectedDifferencesProduct, final long expectedArrangements)
             throws FileNotFoundException {
         List<Integer> testAdapters = readAdapters(filename);
         testDifferencesProduct(testAdapters, expectedDifferencesProduct);
