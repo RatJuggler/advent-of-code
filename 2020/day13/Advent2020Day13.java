9eb03a5a96da3bf6346b8b435511d0e9b1f83afb
John Chase <ratteal@gmail.com>
2020-12-13 09:24:19 +0000

    Read input file.

--- 2020/day13/Advent2020Day13.java
+++ 2020/day13/Advent2020Day13.java
@@ -1,5 +1,11 @@
 package day13;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 
 class Notes {
 
@@ -11,9 +17,13 @@ class Notes {
         this.buses = buses;
     }
 
-    static Notes fromFile(final String filename) {
-        int startTime = 0;
-        int[] buses = new int[0];
+    static Notes fromFile(final String filename) throws IOException {
+        List<String> notes = Files.readAllLines(Paths.get(filename));
+        int startTime = Integer.parseInt(notes.get(0));
+        int[] buses = Arrays.stream(notes.get(1).split(","))
+                .filter("x"::equals)
+                .mapToInt(Integer::parseInt)
+                .toArray();
         return new Notes(startTime, buses);
     }
 }
@@ -25,14 +35,14 @@ public class Advent2020Day13 {
         return 0;
     }
 
-    private static void testFindDepartureTime() {
+    private static void testFindDepartureTime() throws IOException {
         int expectedTime = 944;
         Notes notes = Notes.fromFile("2020/day13/test13a.txt");
         int actualTime = findDepartureTime(notes);
         assert actualTime == expectedTime : String.format("Expected departure time to be %d not %d!%n", expectedTime, actualTime);
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
         testFindDepartureTime();
     }
 }
