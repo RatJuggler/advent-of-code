3d80a995ef2d278445a077bb2fe4ffb7c6207654
John Chase <ratteal@gmail.com>
2020-12-13 09:47:07 +0000

    Part 1 corrected test passing and solution.

--- 2020/day13/Advent2020Day13.java
+++ 2020/day13/Advent2020Day13.java
@@ -31,30 +31,32 @@ class Notes {
 
 public class Advent2020Day13 {
 
-    private static int findDepartureTime(final Notes notes) {
+    private static int findWaitProduct(final Notes notes) {
         int time = notes.startTime;
-        int departureFound = 0;
+        int busFound = 0;
         do {
             for (int i = 0; i < notes.buses.length; i++) {
                 int nextDepart = (time / notes.buses[i]) * notes.buses[i];
                 if (nextDepart == time) {
-                    departureFound = time;
+                    busFound = notes.buses[i];
                     break;
                 }
             }
             time++;
-        } while (departureFound == 0);
-        return departureFound;
+        } while (busFound == 0);
+        return (time - 1 - notes.startTime) * busFound;
     }
 
     private static void testFindDepartureTime() throws IOException {
-        int expectedTime = 944;
+        int expectedTime = 295;
         Notes notes = Notes.fromFile("2020/day13/test13a.txt");
-        int actualTime = findDepartureTime(notes);
-        assert actualTime == expectedTime : String.format("Expected departure time to be %d not %d!%n", expectedTime, actualTime);
+        int actualTime = findWaitProduct(notes);
+        assert actualTime == expectedTime : String.format("Expected wait product to be %d not %d!%n", expectedTime, actualTime);
     }
 
     public static void main(final String[] args) throws IOException {
         testFindDepartureTime();
+        Notes notes = Notes.fromFile("2020/day13/input13.txt");
+        System.out.printf("Day 13, Part 1 earliest wait product is %d.%n", findWaitProduct(notes));
     }
 }
