39677a7b1f080c6c6a8f0e480fb066f389fbfcd4
John Chase <ratteal@gmail.com>
2020-12-15 09:32:47 +0000

    Day 15, part 2 tests too slow.

--- 2020/day15/Advent2020Day15.java
+++ 2020/day15/Advent2020Day15.java
@@ -1,7 +1,6 @@
 package day15;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -10,9 +9,9 @@ import java.util.stream.IntStream;
 
 public class Advent2020Day15 {
 
-    private static int numberGame(final String starting) {
+    private static int numberGame(final String starting, final int toTurn) {
         List<Integer> spoken = Arrays.stream(starting.split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        while (spoken.size() < 2020) {
+        while (spoken.size() < toTurn) {
             Integer lastSpoken = spoken.get(spoken.size() - 1);
             int[] occurrences = IntStream.range(0, spoken.size())
                     .filter(i -> Objects.equals(lastSpoken, spoken.get(i)))
@@ -26,19 +25,31 @@ public class Advent2020Day15 {
         return spoken.get(spoken.size() - 1);
     }
 
-    private static void testNumberGame(final String starting, final int expected) {
-        int actual = numberGame(starting);
+    private static void testNumberGame2020(final String starting, final int expected) {
+        int actual = numberGame(starting, 2020);
+        assert actual == expected : String.format("Expected number spoken to be %d not %d!%n", expected, actual);
+    }
+
+    private static void testNumberGame30000000(final String starting, final int expected) {
+        int actual = numberGame(starting, 30000000);
         assert actual == expected : String.format("Expected number spoken to be %d not %d!%n", expected, actual);
     }
 
     public static void main(final String[] args) {
-        testNumberGame("0,3,6", 436);
-        testNumberGame("1,3,2", 1);
-        testNumberGame("2,1,3", 10);
-        testNumberGame("1,2,3", 27);
-        testNumberGame("2,3,1", 78);
-        testNumberGame("3,2,1", 438);
-        testNumberGame("3,1,2", 1836);
-        System.out.printf("Day 15, Part 1, last number spoken is %s\n", numberGame("11,18,0,20,1,7,16"));
+        testNumberGame2020("0,3,6", 436);
+        testNumberGame2020("1,3,2", 1);
+        testNumberGame2020("2,1,3", 10);
+        testNumberGame2020("1,2,3", 27);
+        testNumberGame2020("2,3,1", 78);
+        testNumberGame2020("3,2,1", 438);
+        testNumberGame2020("3,1,2", 1836);
+        System.out.printf("Day 15, Part 1, last number spoken is %s\n", numberGame("11,18,0,20,1,7,16", 2020));
+        testNumberGame30000000("0,3,6", 175594);
+        testNumberGame30000000("1,3,2", 2578);
+        testNumberGame30000000("2,1,3", 3544142);
+        testNumberGame30000000("1,2,3", 261214);
+        testNumberGame30000000("2,3,1", 6895259);
+        testNumberGame30000000("3,2,1", 18);
+        testNumberGame30000000("3,1,2", 362);
     }
 }
