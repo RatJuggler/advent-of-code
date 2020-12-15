a2e684b5d61e8a00112b5c8fdf9715849cbff99d
John Chase <ratteal@gmail.com>
2020-12-15 10:52:44 +0000

    Day 15, part 2 refactor, minor speed improvement.

--- 2020/day15/Advent2020Day15.java
+++ 2020/day15/Advent2020Day15.java
@@ -1,6 +1,5 @@
 package day15;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,26 +7,49 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 
+class Indexes {
+
+    private int current;
+    private int previous = -1;
+
+    Indexes(final int current) {
+        this.current = current;
+    }
+
+    boolean firstIndex() {
+        return this.previous == -1;
+    }
+
+    int newLastSpoken() {
+        return current - previous;
+    }
+
+    void add(final int newIndex) {
+        this.previous = this.current;
+        this.current = newIndex;
+    }
+}
+
+
 public class Advent2020Day15 {
 
     private static int numberGame(final String starting, final int toTurn) {
         List<Integer> start = Arrays.stream(starting.split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        Map<Integer, List<Integer>> spoken = new HashMap<>();
-        for (int i = 0; i < start.size(); i++) {
-            List<Integer> indexes = new ArrayList<>();
-            indexes.add(i);
-            spoken.put(start.get(i), indexes);
-        }
+        Map<Integer, Indexes> spoken = new HashMap<>();
+        for (int i = 0; i < start.size(); i++)
+            spoken.put(start.get(i), new Indexes(i));
         int index = start.size();
         Integer lastSpoken = start.get(index - 1);
+        Indexes indexes = spoken.get(lastSpoken);
         while (index < toTurn) {
-            List<Integer> occurrences = spoken.get(lastSpoken);
-            if (occurrences.size() == 1)
-                lastSpoken = 0;
-            else
-                lastSpoken = occurrences.get(occurrences.size() - 1) - occurrences.get(occurrences.size() - 2);
-            occurrences = spoken.computeIfAbsent(lastSpoken, o -> new ArrayList<>());
-            occurrences.add(index);
+            lastSpoken = indexes.firstIndex() ? 0 : indexes.newLastSpoken();
+            indexes = spoken.get(lastSpoken);
+            if (indexes != null) {
+                indexes.add(index);
+            } else {
+                indexes = new Indexes(index);
+                spoken.put(lastSpoken, indexes);
+            }
             index++;
         }
         return lastSpoken;
@@ -44,6 +66,7 @@ public class Advent2020Day15 {
     }
 
     public static void main(final String[] args) {
+        long startTime = System.currentTimeMillis();
         testNumberGame2020("0,3,6", 436);
         testNumberGame2020("1,3,2", 1);
         testNumberGame2020("2,1,3", 10);
@@ -60,5 +83,6 @@ public class Advent2020Day15 {
         testNumberGame30000000("3,2,1", 18);
         testNumberGame30000000("3,1,2", 362);
         System.out.printf("Day 15, Part 2, last number spoken is %s\n", numberGame("11,18,0,20,1,7,16", 30000000));
+        System.out.println((System.currentTimeMillis() - startTime) / 1000);
     }
 }
