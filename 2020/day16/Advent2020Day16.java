c1ef93be24a3be04643ca5608a61b395c50c4faa
John Chase <ratteal@gmail.com>
2020-12-16 13:36:59 +0000

    Day 16, part 2 test partially working.

--- 2020/day16/Advent2020Day16.java
+++ 2020/day16/Advent2020Day16.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 class Parser {
@@ -54,7 +56,7 @@ class Range {
 
 class FieldRule {
 
-    private final String name;
+    final String name;
     private final List<Range> ranges;
 
     FieldRule(final String name, final List<Range> ranges) {
@@ -110,6 +112,14 @@ class Ticket {
         }
         return errorRate;
     }
+
+    List<Integer> validFields(final FieldRule fieldRule, final Collection<Integer> alreadyClassified) {
+        List<Integer> validFields = new ArrayList<>();
+        for (int i = 0; i < this.fields.size(); i++) {
+            if (!alreadyClassified.contains(i) && fieldRule.inRange(this.fields.get(i))) validFields.add(i);
+        }
+        return validFields;
+    }
 }
 
 
@@ -153,37 +163,101 @@ class TicketScanner {
         return new TicketScanner(fieldRules, myTicket, otherTickets);
     }
 
-    int errorRate() {
+    int totalErrorRate() {
         return this.otherTickets.stream().mapToInt(t -> t.errorRate(this.fieldRules)).sum();
     }
 
+    int myDepartureFieldProduct() {
+        return 0;
+    }
+
     Map<String, Integer> classifyFields() {
-        return new HashMap<>();
+        Map<String, Integer> classifiedFields = new HashMap<>();
+        List<Ticket> validTickets = this.otherTickets.stream().filter(t -> t.errorRate(this.fieldRules) == 0).collect(Collectors.toList());
+        List<String> fields = this.fieldRules.stream().map(r -> r.name).collect(Collectors.toList());
+        while (fields.size() > 0) {
+            List<FieldRule> remainingFields = this.fieldRules.stream().filter(r -> fields.contains(r.name)).collect(Collectors.toList());
+            for (FieldRule fieldRule : remainingFields) {
+                List<Integer> validFields = IntStream.range(0, this.fieldRules.size()).boxed().collect(Collectors.toList());
+                for (Ticket ticket : validTickets) {
+                    List<Integer> ticketValidFields = ticket.validFields(fieldRule, classifiedFields.values());
+                    validFields = validFields.stream().filter(ticketValidFields::contains).collect(Collectors.toList());
+                }
+                if (validFields.size() == 1) {
+                    classifiedFields.put(fieldRule.name, validFields.get(0));
+                    fields.remove(fieldRule.name);
+                }
+            }
+        }
+        return classifiedFields;
     }
 }
 
+// class: 0-1 or 4-19
+// row: 0-5 or 8-19
+// seat: 0-13 or 16-19
+//
+// your ticket:
+// 11,12,13
+//
+// nearby tickets:
+// 3,9,18
+// 15,1,5
+// 5,14,9
+
+// class
+// 3,9,18 -> 1,2
+// 15,1,5 -> 0,1,2
+// 5,14,9 -> 0,1,2
+
+// row
+// 3,9,18 -> 0,1,2
+// 15,1,5 -> 0,1,2
+// 5,14,9 -> 0,1,2
+
+// seat
+// 3,9,18 -> 0,1,2
+// 15,1,5 -> 1,2
+// 5,14,9 -> 0,2
+
+// seat => 2
+
+// class
+// 3,9,x  -> 1
+// 15,1,x -> 0,1
+// 5,14,x -> 0,1
+
+// row
+// 3,9,x  -> 0,1
+// 15,1,x -> 0,1
+// 5,14,x -> 0,1
+
+// class = 0
+
+// row = 1
 
 public class Advent2020Day16 {
 
     public static void testTickerScannerErrorRate() {
         int expectedRate = 71;
         TicketScanner scanner = TicketScanner.fromFile("2020/day16/test16a.txt");
-        int actualRate = scanner.errorRate();
+        int actualRate = scanner.totalErrorRate();
         assert actualRate == expectedRate : String.format("Expected ticket error rate to be %d not %d!%n", expectedRate, actualRate);
     }
 
-    public static void testTickerScannerFieldClassifier() {
+    public static void testTickerScannerMyDepartureFieldProduct() {
         TicketScanner scanner = TicketScanner.fromFile("2020/day16/test16b.txt");
         Map<String, Integer> myFields = scanner.classifyFields();
-        assert myFields.get("class") == 12 : "Expected 'class' field to be 12!";
-        assert myFields.get("row") == 11 : "Expected 'row' field to be 11!";
-        assert myFields.get("seat") == 13 : "Expected 'seat' field to be 13!";
+        assert myFields.get("class") == 1 : "Expected 'class' field to be 1!";
+        assert myFields.get("row") == 0 : "Expected 'row' field to be 0!";
+        assert myFields.get("seat") == 2 : "Expected 'seat' field to be 2!";
     }
 
     public static void main(final String[] args) {
         testTickerScannerErrorRate();
+        testTickerScannerMyDepartureFieldProduct();
         TicketScanner scanner = TicketScanner.fromFile("2020/day16/input16.txt");
-        System.out.printf("Day 16, Part 1, ticket error rate is %s\n", scanner.errorRate());
-        testTickerScannerFieldClassifier();
+        System.out.printf("Day 16, Part 1, ticket error rate is %s\n", scanner.totalErrorRate());
+        System.out.printf("Day 16, Part 2, my departure field product is %s\n", scanner.myDepartureFieldProduct());
     }
 }
