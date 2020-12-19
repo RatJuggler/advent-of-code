1f4a1e30847df574a64c9cb1ee99f61ab4cf1084
John Chase <ratteal@gmail.com>
2020-12-19 09:54:06 +0000

    Day 19, part 1 start to parse the file.

--- 2020/day19/Advent2020Day19.java
+++ 2020/day19/Advent2020Day19.java
@@ -1,12 +1,68 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+class Rule {
+
+    private final String rule;
+
+    Rule(final String rule) {
+        this.rule = rule;
+    }
+
+    static Rule fromString(final String ruleToParse) {
+        return new Rule(ruleToParse);
+    }
+
+    static List<Rule> fromList(final List<String> ticketsToParse) {
+        return ticketsToParse.stream()
+                .map(Rule::fromString)
+                .collect(Collectors.toList());
+    }
+}
+
 
 class MessageValidator {
 
-    MessageValidator() {}
+    private final List<Rule> rules;
+    private final List<String> messages;
+
+    MessageValidator(final List<Rule> rules, final List<String> messages) {
+        this.rules = rules;
+        this.messages = messages;
+    }
+
+    private static List<List<String>> readFileSections(final String filename) {
+        List<List<String>> fileSections = new ArrayList<>();
+        List<String> section = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.length() != 0) {
+                    section.add(line);
+                } else {
+                    fileSections.add(section);
+                    section = new ArrayList<>();
+                }
+            }
+            fileSections.add(section);
+        } catch (FileNotFoundException fnf) {
+            throw new IllegalArgumentException("Unable to read message file!", fnf);
+        }
+        return fileSections;
+    }
 
     static MessageValidator fromFile(final String filename) {
-        return new MessageValidator();
+        List<List<String>> fileSections = readFileSections(filename);
+        List<Rule> rules = Rule.fromList(fileSections.get(0));
+        List<String> messages = fileSections.get(1);
+        return new MessageValidator(rules, messages);
     }
 
     int validate() {
