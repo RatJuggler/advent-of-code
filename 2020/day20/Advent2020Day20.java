440844ca631431faf31aff95c3be8b8fdd5b8796
John Chase <ratteal@gmail.com>
2020-12-20 10:21:51 +0000

    Day 20, extract tile sides and use a simpler test file to start with.

--- 2020/day20/Advent2020Day20.java
+++ 2020/day20/Advent2020Day20.java
@@ -14,25 +14,38 @@ class ImageTile {
 
     private final long id;
     private final List<String> tile;
+    private final int size;
+    private String top;
+    private String bottom;
+    private String left;
+    private String right;
 
     ImageTile(final long id, final List<String> tile) {
         this.id = id;
         this.tile = Collections.unmodifiableList(tile);
+        this.size = this.tile.size();
+        this.top = tile.get(0);
+        this.bottom = tile.get(this.size - 1);
+        StringBuilder sbLeft = new StringBuilder(this.size);
+        StringBuilder sbRight = new StringBuilder(this.size);
+        for (int i = 0; i < this.size; i++) {
+            sbLeft.append(this.tile.get(i).charAt(0));
+            sbRight.append(this.tile.get(i).charAt(this.size - 1));
+        }
+        this.left = sbLeft.toString();
+        this.right = sbRight.toString();
     }
 
     private static long parseTileId(final String tileId) {
         String pattern = "^Tile (?<id>\\d+):$";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(tileId);
-        if (!m.find()) {
-            throw new IllegalStateException("Unable to parse tile id: " + tileId);
-        }
+        if (!m.find()) throw new IllegalStateException("Unable to parse tile id: " + tileId);
         return Long.parseLong(m.group("id"));
     }
 
     static ImageTile fromStrings(final List<String> tile) {
-        long id = parseTileId(tile.get(0));
-        tile.remove(0);
+        long id = parseTileId(tile.remove(0));
         return new ImageTile(id, tile);
     }
 }
@@ -61,7 +74,7 @@ class ImageAssembler {
             }
             tiles.add(ImageTile.fromStrings(tile));
         } catch (FileNotFoundException fnf) {
-            throw new IllegalArgumentException("Unable to read message file!", fnf);
+            throw new IllegalArgumentException("Unable to read tiles file!", fnf);
         }
         return tiles;
     }
@@ -83,15 +96,15 @@ public class Advent2020Day20 {
         return ImageAssembler.fromFile(filename).assemble();
     }
 
-    private static void testImageAssembler() {
-        long expectedCornerProduct = 20899048083289L;
-        long actualCornerProduct = assembleTiles("2020/day20/test20a.txt");
+    private static void testImageAssembler(final String filename, final long expectedCornerProduct) {
+        long actualCornerProduct = assembleTiles(filename);
         assert actualCornerProduct == expectedCornerProduct :
                 String.format("Expected assembled corner product to be %d not %d!%n", expectedCornerProduct, actualCornerProduct);
     }
 
     public static void main(final String[] args) {
-        testImageAssembler();
+        testImageAssembler("2020/day20/test20a.txt", 2971L * 13L * 33L * 31L);
+        testImageAssembler("2020/day20/test20b.txt", 1951L * 3079L * 1171L * 2971L);
         System.out.printf("Day 20, part 1, corner product is %d.%n", assembleTiles("2020/day20/input20.txt"));
     }
 }
