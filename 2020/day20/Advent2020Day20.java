adc54dfd573af26c00e55a572cd72f98641d78c3
John Chase <ratteal@gmail.com>
2020-12-20 09:08:25 +0000

    Day 20, skeleton code to read tiles.

--- 2020/day20/Advent2020Day20.java
+++ 2020/day20/Advent2020Day20.java
@@ -1,12 +1,54 @@
 package day20;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+
+class ImageTile {
+
+    private final List<String> tile;
+
+    ImageTile(final List<String> tile) {
+        this.tile = Collections.unmodifiableList(tile);
+    }
+}
+
 
 class ImageAssembler {
 
-    ImageAssembler() {}
+    private final List<ImageTile> tiles;
+
+    ImageAssembler(final List<ImageTile> tiles) {
+        this.tiles = Collections.unmodifiableList(tiles);
+    }
+
+    private static List<ImageTile> readImageTiles(final String filename) {
+        List<ImageTile> tiles = new ArrayList<>();
+        List<String> tile = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.length() != 0) {
+                    tile.add(line);
+                } else {
+                    tiles.add(new ImageTile(tile));
+                    tile = new ArrayList<>();
+                }
+            }
+            tiles.add(new ImageTile(tile));
+        } catch (FileNotFoundException fnf) {
+            throw new IllegalArgumentException("Unable to read message file!", fnf);
+        }
+        return tiles;
+    }
 
     static ImageAssembler fromFile(final String filename) {
-        return new ImageAssembler();
+        List<ImageTile> tiles = readImageTiles(filename);
+        return new ImageAssembler(tiles);
     }
 
     long assemble() {
@@ -17,15 +59,19 @@ class ImageAssembler {
 
 public class Advent2020Day20 {
 
+    private static long assembleTiles(final String filename) {
+        return ImageAssembler.fromFile(filename).assemble();
+    }
+
     private static void testImageAssembler() {
         long expectedCornerProduct = 20899048083289L;
-        ImageAssembler assembler = ImageAssembler.fromFile("2020/day20/input20.txt");
-        long actualCornerProduct = assembler.assemble();
+        long actualCornerProduct = assembleTiles("2020/day20/test20a.txt");
         assert actualCornerProduct == expectedCornerProduct :
                 String.format("Expected assembled corner product to be %d not %d!%n", expectedCornerProduct, actualCornerProduct);
     }
 
     public static void main(final String[] args) {
         testImageAssembler();
+        System.out.printf("Day 20, part 1, corner product is %d.%n", assembleTiles("2020/day20/input20.txt"));
     }
 }
